76829545d9c0afaa67b34edd362b958314c3621d
oridim <125223080+oridim@users.noreply.github.com>
2025-05-31 04:44:27 -0400

    feat: added a virtual identifiable table for typing purposes

---
+++ src/ypiretis/app/.server/database/tables/identifiables_table.ts
@@ -0,0 +1,24 @@
+import {integer, sqliteTable} from "drizzle-orm/sqlite-core";
+
+// **IMPORTANT:** This is a dummy table used for typing purposes only! Do NOT
+// use this table when querying the database!
+
+// **HACK:** I would use types instead of creating an object for this generic
+// typing... but... that seemed complicated since Drizzle does not provide a
+// straight-forward way of constructing the required typings without getting
+// knee-deep into its internals. (that I could see)
+//
+// So, we'll use this minimum columns unregistered table to create our generic
+// interface.
+const IDENTIFIABLES_TABLE = sqliteTable(
+    // **HACK:** The table name is treated as a constant literal if we do not
+    // broader its typing here.
+    "identifiables" as string,
+    {
+        id: integer("id").primaryKey({autoIncrement: true}),
+    },
+);
+
+export type IIdentifiablesTable = typeof IDENTIFIABLES_TABLE;
+
+export default IDENTIFIABLES_TABLE;

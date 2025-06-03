1bf276694691b81a121c58738f384eed369bcefe
oridim <125223080+oridim@users.noreply.github.com>
2025-06-03 16:41:15 -0400

    feat: export public API

---
+++ packages/ypiretis/app/.server/services/room_service/index.ts
@@ -0,0 +1,21 @@
+export type * from "./attendee_user";
+export {ATTENDEE_STATES, isAttendeeUser} from "./attendee_user";
+
+export type * from "./display_entity";
+export {isDisplayEntity} from "./display_entity";
+
+export type * from "./entity";
+export {
+    ENTITY_STATES,
+    EntityDisposed,
+    InvalidEntityTypeError,
+    isEntity,
+} from "./entity";
+
+export type * from "./room";
+export {ROOM_STATES, RoomDisposedError} from "./room";
+
+export type * from "./user";
+export {isUser} from "./user";
+
+export * from "./service";

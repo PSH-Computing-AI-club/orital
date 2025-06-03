766ddb4a34e7f84f93456eb835f0439cc6c499d2
oridim <125223080+oridim@users.noreply.github.com>
2025-06-03 07:26:31 -0400

    chore: bikeshedding alphabetization

--- packages/ypiretis/app/.server/utils/crypto.ts
+++ packages/ypiretis/app/.server/utils/crypto.ts
@@ -4,13 +4,6 @@ const {CryptoHasher} = Bun;
 
 const {SECRET_KEY, SECRET_SALT} = ENVIRONMENT;
 
-export function hashSecret(secret: string): string {
-    const hasher = new CryptoHasher("sha256", SECRET_KEY.expose());
-
-    hasher.update(secret + SECRET_SALT, "utf-8");
-    return hasher.digest("hex");
-}
-
 export function generatePIN(): string {
     const buffer = new Uint8Array(6);
 
@@ -19,4 +12,11 @@ export function generatePIN(): string {
     return Array.from(buffer)
         .map((value) => (value % 36).toString(36).toUpperCase())
         .join("");
+}
+
+export function hashSecret(secret: string): string {
+    const hasher = new CryptoHasher("sha256", SECRET_KEY.expose());
+
+    hasher.update(secret + SECRET_SALT, "utf-8");
+    return hasher.digest("hex");
 }

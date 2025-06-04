b964d37bc1f4868b1e45d791c3e3d2ff243f0d5e
oridim <125223080+oridim@users.noreply.github.com>
2025-06-04 11:57:17 -0400

    feat: scaffolded presenter view

---
+++ packages/ypiretis/app/routes/rooms.presenter._index.tsx
@@ -0,0 +1,52 @@
+import {Strong, Text} from "@chakra-ui/react";
+
+import {useEffect} from "react";
+
+import {requireAuthenticatedSession} from "~/.server/services/users_service";
+
+import useEventSource from "~/hooks/event_source";
+
+import {Route} from "./+types/rooms.presenter._index";
+
+export async function loader(loaderArgs: Route.LoaderArgs) {
+    const {request} = loaderArgs;
+
+    await requireAuthenticatedSession(request);
+}
+
+export function HydrateFallback() {
+    return (
+        <>
+            <noscript>
+                <Text>
+                    JavaScript is <Strong color="red.solid">required</Strong> to
+                    log-in.
+                </Text>
+            </noscript>
+
+            <Text>Loading...</Text>
+        </>
+    );
+}
+
+export default function RoomsPresenter() {
+    const message = useEventSource("/rooms/presenter/events", {
+        init: {
+            async onopen(response) {
+                if (!response.ok) {
+                    // **TODO:** do something here
+                }
+            },
+        },
+    });
+
+    useEffect(() => {
+        if (!message) {
+            return;
+        }
+
+        console.log({message});
+    }, [message]);
+
+    return <>Stuff will happun here!</>;
+}

392825e9ab0fde5edee5b6797917103c21ac155b
oridim <125223080+oridim@users.noreply.github.com>
2025-06-04 09:37:10 -0400

    feat: rooms are created when the presenter subscribes to a room event stream

---
+++ packages/ypiretis/app/routes/rooms_.presenter_.events.tsx
@@ -0,0 +1,31 @@
+import {eventStream} from "remix-utils/sse/server";
+
+import {insertOneLive} from "~/.server/services/room_service";
+import {requireAuthenticatedSession} from "~/.server/services/users_service";
+
+import type {Route} from "./+types/rooms_.presenter_.events";
+
+export async function loader(loaderArgs: Route.LoaderArgs) {
+    const {request} = loaderArgs;
+
+    const {signal} = request;
+
+    const {identifiable: user} = await requireAuthenticatedSession(request);
+
+    const room = await insertOneLive({
+        presenter: user,
+    });
+
+    return eventStream(signal, (send, abort) => {
+        room.addPresenter({
+            abort,
+            send,
+        });
+
+        return () => {
+            // **NOTE:** Rooms only exist for the lifetime of presenter's
+            // connection to keep things simple.
+            room.dispose();
+        };
+    });
+}

ebfd87d90afd880e7809307e26e2330516c55f5a
oridim <125223080+oridim@users.noreply.github.com>
2025-06-06 15:08:59 -0400

    feat: added backend support for initial batch of presenter events

--- packages/ypiretis/app/.server/services/room_service/presenter_user.ts
+++ packages/ypiretis/app/.server/services/room_service/presenter_user.ts
@@ -1,9 +1,79 @@
+import type {IEntityNetworkEvent} from "./entity";
+import {SYMBOL_ENTITY_ON_DISPOSE} from "./entity";
+
+import {isAttendeeUser} from "./attendee_user";
+import {isDisplayEntity} from "./display_entity";
+import type {IRoomStates} from "./room";
 import type {IUser, IUserOptions} from "./user";
 import makeUser from "./user";
 
 const SYMBOL_PRESENTER_USER_BRAND: unique symbol = Symbol();
 
-export type IPresenterUserNetworkEvents = null;
+export const PRESENTER_ENTITY_ID = 1;
+
+export type IPresenterUserNetworkEvents =
+    | IPresenterRoomAttendeeAddedEvent
+    | IPresenterRoomAttendeeDisposedEvent
+    | IPresenterRoomDisplayAddedEvent
+    | IPresenterRoomDisplayDisposedEvent
+    | IPresenterRoomPINUpdateEvent
+    | IPresenterRoomStateUpdateEvent
+    | IPresenterRoomTitleUpdateEvent;
+
+export type IPresenterRoomAttendeeAddedEvent = IEntityNetworkEvent<
+    "room.attendeeAdded",
+    {
+        readonly accountID: string;
+
+        readonly entityID: number;
+
+        readonly firstName: string;
+
+        readonly lastName: string;
+    }
+>;
+
+export type IPresenterRoomAttendeeDisposedEvent = IEntityNetworkEvent<
+    "room.attendeeDisposed",
+    {
+        readonly entityID: number;
+    }
+>;
+
+export type IPresenterRoomDisplayAddedEvent = IEntityNetworkEvent<
+    "room.displayAdded",
+    {
+        readonly entityID: number;
+    }
+>;
+
+export type IPresenterRoomDisplayDisposedEvent = IEntityNetworkEvent<
+    "room.displayDisposed",
+    {
+        readonly entityID: number;
+    }
+>;
+
+export type IPresenterRoomPINUpdateEvent = IEntityNetworkEvent<
+    "room.pinUpdate",
+    {
+        readonly pin: string;
+    }
+>;
+
+export type IPresenterRoomStateUpdateEvent = IEntityNetworkEvent<
+    "room.stateUpdate",
+    {
+        readonly state: IRoomStates;
+    }
+>;
+
+export type IPresenterRoomTitleUpdateEvent = IEntityNetworkEvent<
+    "room.titleUpdate",
+    {
+        readonly title: string;
+    }
+>;
 
 export interface IPresenterUserOptions extends IUserOptions {}
 
@@ -24,11 +94,124 @@ export function isPresenterUser(value: unknown): value is IPresenterUser {
 export default function makePresenterUser(
     options: IPresenterUserOptions,
 ): IPresenterUser {
+    const {room} = options;
+
     const user = makeUser<IPresenterUserNetworkEvents>(options);
 
-    return {
+    const presenter = {
         ...user,
 
         [SYMBOL_PRESENTER_USER_BRAND]: true,
-    };
+
+        [SYMBOL_ENTITY_ON_DISPOSE]() {
+            entityAddedSubscription.dispose();
+            entityDisposedSubscription.dispose();
+
+            pinUpdateSubscription.dispose();
+            stateUpdateSubscription.dispose();
+            titleUpdateSubscription.dispose();
+        },
+    } satisfies IPresenterUser;
+
+    const entityAddedSubscription = room.EVENT_ENTITY_ADDED.subscribe(
+        (event) => {
+            const {entity} = event;
+
+            if (isAttendeeUser(entity)) {
+                const {id, user} = entity;
+                const {accountID, firstName, lastName} = user;
+
+                presenter._dispatch({
+                    event: "room.attendeeAdded",
+
+                    data: {
+                        accountID,
+                        entityID: id,
+                        firstName,
+                        lastName,
+                    },
+                });
+            } else if (isDisplayEntity(entity)) {
+                const {id} = entity;
+
+                presenter._dispatch({
+                    event: "room.displayAdded",
+
+                    data: {
+                        entityID: id,
+                    },
+                });
+            }
+        },
+    );
+
+    const entityDisposedSubscription = room.EVENT_ENTITY_DISPOSED.subscribe(
+        (event) => {
+            const {entity} = event;
+
+            if (isAttendeeUser(entity)) {
+                const {id} = entity;
+
+                presenter._dispatch({
+                    event: "room.attendeeDisposed",
+
+                    data: {
+                        entityID: id,
+                    },
+                });
+            } else if (isDisplayEntity(entity)) {
+                const {id} = entity;
+
+                presenter._dispatch({
+                    event: "room.displayDisposed",
+
+                    data: {
+                        entityID: id,
+                    },
+                });
+            }
+        },
+    );
+
+    const pinUpdateSubscription = room.EVENT_PIN_UPDATE.subscribe((event) => {
+        const {newPIN} = event;
+
+        presenter._dispatch({
+            event: "room.pinUpdate",
+
+            data: {
+                pin: newPIN,
+            },
+        });
+    });
+
+    const stateUpdateSubscription = room.EVENT_STATE_UPDATE.subscribe(
+        (event) => {
+            const {newState} = event;
+
+            presenter._dispatch({
+                event: "room.stateUpdate",
+
+                data: {
+                    state: newState,
+                },
+            });
+        },
+    );
+
+    const titleUpdateSubscription = room.EVENT_TITLE_UPDATE.subscribe(
+        (event) => {
+            const {newTitle} = event;
+
+            presenter._dispatch({
+                event: "room.titleUpdate",
+
+                data: {
+                    title: newTitle,
+                },
+            });
+        },
+    );
+
+    return presenter;
 }

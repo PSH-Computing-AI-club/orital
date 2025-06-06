246eb1c8b6e51a1bf1536881eb5d8ad145568d31
oridim <125223080+oridim@users.noreply.github.com>
2025-06-06 11:51:31 -0400

    feat: added icons needed for upcoming design

---
+++ packages/ypiretis/app/components/icons/more_vertical_icon.tsx
@@ -0,0 +1,9 @@
+import Icon from "./icon";
+
+export default function MoreVerticalIcon() {
+    return (
+        <Icon>
+            <path d="M15 1v6H9V1h6zm-2 2h-2v2h2V3zm2 6v6H9V9h6zm-2 2h-2v2h2v-2zm2 6v6H9v-6h6zm-2 2h-2v2h2v-2z" />
+        </Icon>
+    );
+}
---
+++ packages/ypiretis/app/components/icons/pin_icon.tsx
@@ -0,0 +1,9 @@
+import Icon from "./icon";
+
+export default function PinIcon() {
+    return (
+        <Icon>
+            <path d="M7 2h10v2H7V2zM5 6V4h2v2H5zm0 8H3V6h2v8zm2 2H5v-2h2v2zm2 2H7v-2h2v2zm2 2H9v-2h2v2zm2 0v2h-2v-2h2zm2-2v2h-2v-2h2zm2-2v2h-2v-2h2zm2-2v2h-2v-2h2zm0-8h2v8h-2V6zm0 0V4h-2v2h2zm-5 2h-4v4h4V8z" />
+        </Icon>
+    );
+}

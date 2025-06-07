01124ec23405212bddd5033db4e51a6ca922bab1
oridim <125223080+oridim@users.noreply.github.com>
2025-06-07 15:14:42 -0400

    feat: support synchronized room data for presenter views

---
+++ packages/ypiretis/app/routes/rooms.$roomID.presenter._index.tsx
@@ -0,0 +1,5 @@
+import {Route} from "./+types/rooms.$roomID.presenter._index";
+
+export default function RoomsPresenterIndex(props: Route.ComponentProps) {
+    return <>Stuff will happun here!</>;
+}
---
+++ packages/ypiretis/app/routes/rooms.$roomID.presenter.tsx
@@ -0,0 +1,103 @@
+import {Strong, Text} from "@chakra-ui/react";
+
+import {Outlet, data} from "react-router";
+
+import * as v from "valibot";
+
+import {ROOM_ID_PREFIX} from "~/.server/database/tables/rooms_table";
+
+import {requireAuthenticatedPresenterSession} from "~/.server/services/room_service";
+
+import type {IAttendee, IDisplay} from "~/state/presenter";
+import {PresenterContextProvider} from "~/state/presenter";
+
+import {token} from "~/utils/valibot";
+
+import {Route} from "./+types/rooms.$roomID.presenter";
+
+const LOADER_SCHEMA = v.object({
+    roomID: token(ROOM_ID_PREFIX),
+});
+
+export function clientLoader(loaderArgs: Route.ClientLoaderArgs) {
+    return loaderArgs.serverLoader();
+}
+
+clientLoader.hydrate = true as const;
+
+export async function loader(loaderArgs: Route.LoaderArgs) {
+    const {params, request} = loaderArgs;
+
+    const {output, success} = v.safeParse(LOADER_SCHEMA, params);
+
+    if (!success) {
+        throw data("Bad Request", 400);
+    }
+
+    const {room, user} = await requireAuthenticatedPresenterSession(
+        request,
+        output.roomID,
+    );
+
+    const {attendees, displays, pin, roomID, state, title} = room;
+
+    const initialAttendees = Array.from(attendees.values()).map((attendee) => {
+        const {id: entityID, user} = attendee;
+        const {accountID, firstName, lastName} = user;
+
+        return {
+            accountID,
+            entityID,
+            firstName,
+            lastName,
+        };
+    }) satisfies IAttendee[];
+
+    const initialDisplays = Array.from(displays.values()).map((display) => {
+        const {id: entityID} = display;
+
+        return {
+            entityID,
+        };
+    }) satisfies IDisplay[];
+
+    return {
+        initialRoomData: {
+            attendees: initialAttendees,
+            displays: initialDisplays,
+
+            pin,
+            roomID,
+            state,
+            title,
+        },
+    };
+}
+
+export function HydrateFallback() {
+    return (
+        <>
+            <noscript>
+                <Text>
+                    JavaScript is <Strong color="red.solid">required</Strong> to
+                    use the presenter portal.
+                </Text>
+            </noscript>
+
+            <Text>Loading...</Text>
+        </>
+    );
+}
+
+export default function RoomsPresenterLayout(props: Route.ComponentProps) {
+    const {loaderData} = props;
+    const {initialRoomData} = loaderData;
+
+    return (
+        <>
+            <PresenterContextProvider initialRoomData={initialRoomData}>
+                <Outlet />
+            </PresenterContextProvider>
+        </>
+    );
+}

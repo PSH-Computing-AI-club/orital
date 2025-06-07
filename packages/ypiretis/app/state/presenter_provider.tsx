f6c9bea35ec1f2d2c45ce54611d2d85639df655a
oridim <125223080+oridim@users.noreply.github.com>
2025-06-07 13:01:08 -0400

    feat: added initial provider for presenter data

---
+++ packages/ypiretis/app/state/presenter_provider.tsx
@@ -0,0 +1,99 @@
+import type {PropsWithChildren} from "react";
+import {createContext, useContext, useEffect, useState} from "react";
+
+import type {
+    IRoomStates,
+    IPresenterUserNetworkEvents,
+} from "~/.server/services/room_service";
+
+import useEventSource from "~/hooks/event_source";
+
+const CONTEXT_PRESENTER = createContext<IRoom | null>(null);
+
+export interface IEntity {
+    readonly entityID: number;
+}
+
+export interface IUser extends IEntity {
+    readonly accountID: string;
+
+    readonly entityID: number;
+
+    readonly firstName: string;
+
+    readonly lastName: string;
+}
+
+export interface IDisplay extends IEntity {}
+
+export interface IAttendee extends IUser {}
+
+export interface IRoom {
+    readonly attendees: IAttendee[];
+
+    readonly displays: IDisplay[];
+
+    readonly pin: string;
+
+    readonly roomID: string;
+
+    readonly state: IRoomStates;
+
+    readonly title: string;
+}
+
+export interface IPresenterContextProviderProps extends PropsWithChildren {
+    readonly initialRoomData: IRoom;
+}
+
+export function PresenterContextProvider(
+    props: IPresenterContextProviderProps,
+) {
+    const {children, initialRoomData} = props;
+    const [room, setRoom] = useState<IRoom>(initialRoomData);
+
+    const message = useEventSource(`/rooms/${room.roomID}/presenter/events`, {
+        init: {
+            async onopen(response) {
+                if (!response.ok) {
+                    // **TODO:** error handle here somehow
+                }
+            },
+        },
+    });
+
+    useEffect(() => {
+        if (!message) {
+            return;
+        }
+
+        const event = {
+            event: message.event,
+            data: JSON.parse(message.data),
+        } as IPresenterUserNetworkEvents;
+
+        switch (
+            event.event
+            // **TODO:** do stuff with `setRoom`
+        ) {
+        }
+    }, [message]);
+
+    return (
+        <CONTEXT_PRESENTER.Provider value={room}>
+            {children}
+        </CONTEXT_PRESENTER.Provider>
+    );
+}
+
+export function usePresenterContext(): IRoom {
+    const context = useContext(CONTEXT_PRESENTER);
+
+    if (context === null) {
+        throw new ReferenceError(
+            `bad dispatch to 'usePresenterContext' (not a child of 'PresenterContextProvider')`,
+        );
+    }
+
+    return context;
+}

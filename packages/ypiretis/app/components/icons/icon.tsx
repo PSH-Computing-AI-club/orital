dfe1c041e8f9afcadd79ef9aa6ce846a643df935
oridim <125223080+oridim@users.noreply.github.com>
2025-06-08 15:31:07 -0400

    fix: forgot to actually set the intrinsic size of the SVG

--- packages/ypiretis/app/components/icons/icon.tsx
+++ packages/ypiretis/app/components/icons/icon.tsx
@@ -6,10 +6,16 @@ export type IIconProps = Omit<IIconRootProps, "children">;
 export type IIconRootProps = Omit<IconProps, "as" | "asChild">;
 
 export default function IconRoot(props: IIconRootProps) {
-    const {children, fill = "currentcolor", size = "md", ...rest} = props;
+    const {
+        children,
+        fill = "currentcolor",
+        size = "md",
+        viewBox = "0 0 24 24",
+        ...rest
+    } = props;
 
     return (
-        <Icon as="svg" fill={fill} size={size} {...rest}>
+        <Icon as="svg" viewBox={viewBox} fill={fill} size={size} {...rest}>
             {children}
         </Icon>
     );

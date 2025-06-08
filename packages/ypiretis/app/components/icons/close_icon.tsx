ecbfe4af5fa1385633733e811d15a6e99ce07e4a
oridim <125223080+oridim@users.noreply.github.com>
2025-06-08 16:41:32 -0400

    feat: added needed close icon

---
+++ packages/ypiretis/app/components/icons/close_icon.tsx
@@ -0,0 +1,10 @@
+import type {IIconProps} from "./icon";
+import IconRoot from "./icon";
+
+export default function CloseIcon(props: IIconProps) {
+    return (
+        <IconRoot {...props}>
+            <path d="M5 5h2v2H5V5zm4 4H7V7h2v2zm2 2H9V9h2v2zm2 0h-2v2H9v2H7v2H5v2h2v-2h2v-2h2v-2h2v2h2v2h2v2h2v-2h-2v-2h-2v-2h-2v-2zm2-2v2h-2V9h2zm2-2v2h-2V7h2zm0 0V5h2v2h-2z" />
+        </IconRoot>
+    );
+}

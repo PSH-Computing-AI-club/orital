4b900916716fe57ba6878c38bdb62dbcde85d933
oridim <125223080+oridim@users.noreply.github.com>
2025-06-08 16:41:51 -0400

    feat: first pass at presenter portal sidebar

---
+++ packages/ypiretis/app/components/shell/app_shell.tsx
@@ -0,0 +1,161 @@
+import type {ColorPalette} from "@chakra-ui/react";
+import {
+    Box,
+    Bleed,
+    Button,
+    Container,
+    Image,
+    Flex,
+    VStack,
+} from "@chakra-ui/react";
+
+import type {MouseEventHandler, PropsWithChildren} from "react";
+
+import type {To} from "react-router";
+import {Link} from "react-router";
+
+export interface IAppShellButtonProps extends PropsWithChildren {
+    readonly colorPalette?: ColorPalette;
+
+    readonly onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface IAppShellIconProps extends PropsWithChildren {}
+
+export interface IAppShellLinkProps extends PropsWithChildren {
+    readonly active?: boolean;
+
+    readonly to: To;
+}
+
+export interface IAppShellContainerProps extends PropsWithChildren {}
+
+export interface IAppShellRootProps extends PropsWithChildren {}
+
+export interface IAppShellSidebarProps extends PropsWithChildren {}
+
+function AppShellButton(props: IAppShellButtonProps) {
+    const {children, colorPalette, onClick} = props;
+
+    return (
+        <Button
+            variant="ghost"
+            colorPalette={colorPalette}
+            size="2xs"
+            flexDirection="column"
+            fontWeight="bold"
+            width="full"
+            paddingY="10"
+            gap="2"
+            onClick={onClick}
+        >
+            {children}
+        </Button>
+    );
+}
+
+function AppShellDivider() {
+    return (
+        <Box
+            borderBlockStartColor="border"
+            borderBlockStartStyle="solid"
+            borderBlockStartWidth="thin"
+            marginBlockStart="auto"
+            width="full"
+        />
+    );
+}
+
+function AppShellIcon(props: IAppShellIconProps) {
+    const {children} = props;
+
+    return (
+        <Box width="2.5em" height="2.5em" asChild>
+            {children}
+        </Box>
+    );
+}
+
+function AppShellLink(props: IAppShellLinkProps) {
+    const {active = false, children, to} = props;
+
+    return (
+        <Button
+            variant="ghost"
+            colorPalette={active ? "cyan" : undefined}
+            size="2xs"
+            flexDirection="column"
+            fontWeight="bold"
+            width="full"
+            paddingY="10"
+            gap="2"
+            asChild
+        >
+            <Link to={to}>{children}</Link>
+        </Button>
+    );
+}
+
+function AppShellContainer(props: IAppShellContainerProps) {
+    const {children} = props;
+
+    return <Container>{children}</Container>;
+}
+
+function AppShellSidebar(props: IAppShellSidebarProps) {
+    const {children} = props;
+
+    return (
+        <VStack
+            gap="4"
+            bg="bg"
+            borderInlineEndColor="border"
+            borderInlineEndStyle="solid"
+            borderInlineEndWidth="thin"
+            padding="4"
+            maxInlineSize="max"
+            minInlineSize="max"
+        >
+            <Bleed
+                blockStart="4"
+                inline="4"
+                bg="bg.inverted"
+                borderBlockEndColor="border"
+                borderBlockEndStyle="solid"
+                borderBlockEndWidth="thin"
+                padding="4"
+                alignSelf="stretch"
+            >
+                <Image
+                    src="/images/logo.prompt.webp"
+                    marginInline="auto"
+                    width="16"
+                />
+            </Bleed>
+
+            {children}
+        </VStack>
+    );
+}
+
+function AppShellRoot(props: IAppShellRootProps) {
+    const {children} = props;
+
+    return (
+        <Flex align="stretch" width="dvw" height="dvh">
+            {children}
+        </Flex>
+    );
+}
+
+const AppShell = {
+    Button: AppShellButton,
+    Container: AppShellContainer,
+    Divider: AppShellDivider,
+    Icon: AppShellIcon,
+    Link: AppShellLink,
+    Sidebar: AppShellSidebar,
+    Root: AppShellRoot,
+} as const;
+
+export default AppShell;
--- packages/ypiretis/app/routes/rooms.$roomID.presenter._index.tsx
+++ packages/ypiretis/app/routes/rooms.$roomID.presenter._index.tsx
@@ -1,5 +1,53 @@
+import {Box} from "@chakra-ui/react";
+
+import CloseIcon from "~/components/icons/close_icon";
+import DashboardIcon from "~/components/icons/dashboard_icon";
+import ChartIcon from "~/components/icons/chart_icon";
+import SlidersIcon from "~/components/icons/sliders_icon";
+
+import AppShell from "~/components/shell/app_shell";
+
 import {Route} from "./+types/rooms.$roomID.presenter._index";
 
 export default function RoomsPresenterIndex(props: Route.ComponentProps) {
-    return <>Stuff will happun here!</>;
+    return (
+        <AppShell.Root>
+            <AppShell.Sidebar>
+                <AppShell.Link to="/" active>
+                    <AppShell.Icon>
+                        <DashboardIcon />
+                    </AppShell.Icon>
+                    Dashboard
+                </AppShell.Link>
+
+                <AppShell.Link to="/">
+                    <AppShell.Icon>
+                        <ChartIcon />
+                    </AppShell.Icon>
+                    Polls
+                </AppShell.Link>
+
+                <AppShell.Divider />
+
+                <AppShell.Link to="/">
+                    <AppShell.Icon>
+                        <SlidersIcon />
+                    </AppShell.Icon>
+                    Settings
+                </AppShell.Link>
+
+                <AppShell.Button
+                    colorPalette="red"
+                    onClick={() => console.log("hello world!")}
+                >
+                    <AppShell.Icon>
+                        <CloseIcon />
+                    </AppShell.Icon>
+                    Close Room
+                </AppShell.Button>
+            </AppShell.Sidebar>
+
+            <AppShell.Container>Stuff will happun here!</AppShell.Container>
+        </AppShell.Root>
+    );
 }

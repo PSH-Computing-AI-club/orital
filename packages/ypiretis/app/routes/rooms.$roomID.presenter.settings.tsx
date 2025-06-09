d8783b40aed9796618c57d3dfde483f38602ffd7
oridim <125223080+oridim@users.noreply.github.com>
2025-06-08 21:11:53 -0400

    feat: placeholder pages

---
+++ packages/ypiretis/app/routes/rooms.$roomID.presenter.settings.tsx
@@ -0,0 +1,11 @@
+import AppShell from "~/components/shell/app_shell";
+
+import {Route} from "./+types/rooms.$roomID.presenter._index";
+
+export default function RoomsPresenterIndex(props: Route.ComponentProps) {
+    return (
+        <AppShell.Container>
+            Stuff will happun here at da settings!
+        </AppShell.Container>
+    );
+}

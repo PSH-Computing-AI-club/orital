ffa9875e3e664c81482d86c1fd6eff22ea404a9e
oridim <125223080+oridim@users.noreply.github.com>
2025-06-08 21:14:38 -0400

    feat: update active link on sidebar

--- packages/ypiretis/app/routes/rooms.$roomID.presenter.tsx
+++ packages/ypiretis/app/routes/rooms.$roomID.presenter.tsx
@@ -1,6 +1,6 @@
 import {Strong, Text} from "@chakra-ui/react";
 
-import {Outlet, data} from "react-router";
+import {Outlet, data, useLocation} from "react-router";
 
 import * as v from "valibot";
 
@@ -112,19 +112,29 @@ export default function RoomsPresenterLayout(props: Route.ComponentProps) {
 
     const {roomID} = initialRoomData;
 
+    const {pathname} = useLocation();
+
     return (
         <SessionContextProvider session={session}>
             <PresenterContextProvider initialRoomData={initialRoomData}>
                 <AppShell.Root>
                     <AppShell.Sidebar>
-                        <AppShell.Link to={`/rooms/${roomID}/presenter`} active>
+                        <AppShell.Link
+                            to={`/rooms/${roomID}/presenter`}
+                            active={pathname === `/rooms/${roomID}/presenter`}
+                        >
                             <AppShell.Icon>
                                 <DashboardIcon />
                             </AppShell.Icon>
                             Dashboard
                         </AppShell.Link>
 
-                        <AppShell.Link to={`/rooms/${roomID}/presenter/polls`}>
+                        <AppShell.Link
+                            to={`/rooms/${roomID}/presenter/polls`}
+                            active={
+                                pathname === `/rooms/${roomID}/presenter/polls`
+                            }
+                        >
                             <AppShell.Icon>
                                 <ChartIcon />
                             </AppShell.Icon>
@@ -135,6 +145,10 @@ export default function RoomsPresenterLayout(props: Route.ComponentProps) {
 
                         <AppShell.Link
                             to={`/rooms/${roomID}/presenter/settings`}
+                            active={
+                                pathname ===
+                                `/rooms/${roomID}/presenter/settings`
+                            }
                         >
                             <AppShell.Icon>
                                 <SlidersIcon />

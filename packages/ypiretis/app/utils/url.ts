9bdd71e036206e30fc9571732e9e0a89862a677b
oridim <125223080+oridim@users.noreply.github.com>
2025-06-10 18:04:53 -0400

    feat: updated `buildAppURL` to support RRv7 interfaces

--- packages/ypiretis/app/utils/url.ts
+++ packages/ypiretis/app/utils/url.ts
@@ -1,8 +1,18 @@
 // **IMPORTANT:** Do **NOT** use absolute imports in this module. It is
 // imported by server-only modules.
 
+import type {Location, To} from "react-router";
+
 import {APP_URL} from "./constants";
 
-export function buildAppURL(url: URL | string): URL {
+export function buildAppURL(url: Location | To | URL | string): URL {
+    if (url instanceof URL) {
+        return url;
+    } else if (typeof url === "object") {
+        const {hash = "", pathname = "", search = ""} = url;
+
+        return new URL(`${pathname}${search}${hash}`, APP_URL);
+    }
+
     return new URL(url, APP_URL);
 }

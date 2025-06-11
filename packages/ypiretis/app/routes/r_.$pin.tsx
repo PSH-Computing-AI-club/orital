5744029dc976c99a6296b3ad0c49243982663621
oridim <125223080+oridim@users.noreply.github.com>
2025-06-11 18:46:39 -0400

    feat: quick join by PIN route

---
+++ packages/ypiretis/app/routes/r_.$pin.tsx
@@ -0,0 +1,44 @@
+import {data, redirect} from "react-router";
+
+import * as v from "valibot";
+
+import {ROOM_STATES, findOneLiveByPIN} from "~/.server/services/room_service";
+import {requireAuthenticatedSession} from "~/.server/services/users_service";
+
+import {pin as pinValidator} from "~/utils/valibot";
+
+import {Route} from "./+types/r_.$pin";
+
+const LOADER_PARAMS_SCHEMA = v.object({
+    pin: v.pipe(v.string(), v.length(6), pinValidator),
+});
+
+export async function loader(loaderArgs: Route.LoaderArgs) {
+    const {params, request} = loaderArgs;
+
+    const {output: paramsData, success: isValidParams} = v.safeParse(
+        LOADER_PARAMS_SCHEMA,
+        params,
+    );
+
+    if (!isValidParams) {
+        throw data("Bad Request", 400);
+    }
+
+    await requireAuthenticatedSession(request);
+
+    const {pin} = paramsData;
+    const room = findOneLiveByPIN(pin);
+
+    if (room === null) {
+        throw data("Not Found", {
+            status: 404,
+        });
+    }
+
+    if (room.state === ROOM_STATES.disposed) {
+        throw data("Conflict", 409);
+    }
+
+    return redirect(`/rooms/${room.roomID}/attendee`);
+}

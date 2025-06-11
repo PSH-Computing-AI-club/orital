3bbe8c6662d2c604bc007187dcd3cce1ce756700
oridim <125223080+oridim@users.noreply.github.com>
2025-06-11 17:43:00 -0400

    feat: use extended alphabet for PIN generation

--- packages/ypiretis/app/.server/utils/crypto.ts
+++ packages/ypiretis/app/.server/utils/crypto.ts
@@ -4,13 +4,63 @@ const {CryptoHasher} = Bun;
 
 const {SECRET_KEY, SECRET_SALT} = ENVIRONMENT;
 
+const PIN_ALPHABET = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "&",
+    "?",
+    "=",
+    "+",
+    "~",
+    "*",
+] as const;
+
 export function generatePIN(): string {
     const buffer = new Uint8Array(6);
 
     crypto.getRandomValues(buffer);
 
     return Array.from(buffer)
-        .map((value) => (value % 36).toString(36).toUpperCase())
+        .map((value) => PIN_ALPHABET[value % PIN_ALPHABET.length])
         .join("");
 }
 

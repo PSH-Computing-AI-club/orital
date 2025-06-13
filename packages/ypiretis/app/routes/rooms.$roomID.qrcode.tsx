50c86eb4ebc53871900038162ed828ec9e13994a
oridim <125223080+oridim@users.noreply.github.com>
2025-06-12 22:57:13 -0400

    feat: reactive QR Code route

--- packages/ypiretis/app/routes/rooms.$roomID.qrcode.tsx
+++ packages/ypiretis/app/routes/rooms.$roomID.qrcode.tsx
@@ -1,57 +1,86 @@
 import {
-    Card,
-    Container,
     Flex,
     Heading,
     Highlight,
     Link,
     QrCode,
+    Strong,
     Text,
 } from "@chakra-ui/react";
 
+import type {ShouldRevalidateFunction} from "react-router";
 import {data} from "react-router";
 
 import * as v from "valibot";
 
-import {requireAuthenticatedPresenterSession} from "~/.server/services/room_service";
+import {requireAuthenticatedDisplayConnection} from "~/.server/services/room_service";
+
+import {WebSocketCacheProvider} from "~/hooks/web_socket";
+
+import type {IRoom} from "~/state/display";
+import {DisplayContextProvider, useDisplayContext} from "~/state/display";
 
 import {buildAppURL} from "~/utils/url";
 
 import {Route} from "./+types/rooms.$roomID.qrcode";
 
-const ACTION_PARAMS_SCHEMA = v.object({
+const LOADER_PARAMS_SCHEMA = v.object({
     roomID: v.pipe(v.string(), v.ulid()),
 });
+
+export const shouldRevalidate = ((_revalidateArgs) => {
+    return false;
+}) satisfies ShouldRevalidateFunction;
+
+export function clientLoader(loaderArgs: Route.ClientLoaderArgs) {
+    return loaderArgs.serverLoader();
+}
+
+clientLoader.hydrate = true as const;
 
 export async function loader(loaderArgs: Route.LoaderArgs) {
     const {params, request} = loaderArgs;
 
-    const {output: paramsData, success: isValidParams} = v.safeParse(
-        ACTION_PARAMS_SCHEMA,
-        params,
-    );
+    const {output, success} = v.safeParse(LOADER_PARAMS_SCHEMA, params);
 
-    if (!isValidParams) {
+    if (!success) {
         throw data("Bad Request", 400);
     }
 
-    const {room} = await requireAuthenticatedPresenterSession(
+    const {room} = await requireAuthenticatedDisplayConnection(
         request,
-        paramsData.roomID,
+        output.roomID,
     );
 
-    const {roomID, pin, title} = room;
+    const {pin, roomID, state, title} = room;
 
     return {
-        pin,
-        roomID,
-        title,
+        initialRoomData: {
+            pin,
+            roomID,
+            state,
+            title,
+        } satisfies IRoom,
     };
 }
 
-export default function RoomsQRCode(props: Route.ComponentProps) {
-    const {loaderData} = props;
-    const {pin, roomID, title} = loaderData;
+export function HydrateFallback() {
+    return (
+        <>
+            <noscript>
+                <Text>
+                    JavaScript is <Strong color="red.solid">required</Strong> to
+                    display the QR Code.
+                </Text>
+            </noscript>
+
+            <Text>Loading...</Text>
+        </>
+    );
+}
+
+function QRCodeView() {
+    const {pin, roomID, title} = useDisplayContext();
 
     const roomURL = buildAppURL(`/attendee/${roomID}`).toString();
     const joinURL = buildAppURL(`/r/${pin}`).toString();
@@ -88,10 +117,28 @@ export default function RoomsQRCode(props: Route.ComponentProps) {
             </Flex>
 
             <Text fontWeight="bold" fontSize="2xl">
-                <Link variant="underline" href={joinURL} colorPalette="blue">
+                <Link
+                    variant="underline"
+                    href={joinURL}
+                    target="_blank"
+                    colorPalette="blue"
+                >
                     {joinURL}
                 </Link>
             </Text>
         </Flex>
+    );
+}
+
+export default function RoomsQRCode(props: Route.ComponentProps) {
+    const {loaderData} = props;
+    const {initialRoomData} = loaderData;
+
+    return (
+        <WebSocketCacheProvider>
+            <DisplayContextProvider initialRoomData={initialRoomData}>
+                <QRCodeView />
+            </DisplayContextProvider>
+        </WebSocketCacheProvider>
     );
 }

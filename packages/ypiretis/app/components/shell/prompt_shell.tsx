aa39c32a8d9060153d4a893ac65836085befd6a3
oridim <125223080+oridim@users.noreply.github.com>
2025-06-13 03:47:02 -0400

    refactor: following composition pattern
    Now that I got my bearings, this pattern isn't as scary and is more flexible in the future.

--- packages/ypiretis/app/components/shell/prompt_shell.tsx
+++ packages/ypiretis/app/components/shell/prompt_shell.tsx
@@ -15,26 +15,117 @@ import {APP_NAME, PACKAGE_NAME, PACKAGE_VERSION} from "~/utils/constants";
 
 import type {PropsWithChildren} from "react";
 
-export type IPromptShellProps = PropsWithChildren<{
+export interface IPromptShellTitle {
     readonly color?: ColorPalette;
 
     readonly query?: string;
 
     readonly title: string;
-}>;
+}
 
-export default function PromptShell(props: IPromptShellProps) {
-    const {children, color = "cyan.solid", query, title} = props;
+export interface IPromptShellBodyProps extends PropsWithChildren {}
+
+export interface IPromptShellContainerProps extends PropsWithChildren {}
+
+export interface IPromptShellSidebarProps {}
+
+export interface IPromptShellRootProps extends PropsWithChildren {}
+
+function PromptShellTitle(props: IPromptShellTitle) {
+    const {color = "cyan.solid", query, title} = props;
 
     return (
         <>
-            {
-                // **HACK:** React's special handling of the `<title>` element
-                // requires that it has no child elements. That is, it is only
-                // a singular primitive value.
-            }
             <title>{`${title} :: ${APP_NAME}`}</title>
 
+            <Heading
+                size="4xl"
+                marginBlockStart={{lgDown: "auto"}}
+                maxInlineSize={{smDown: "min"}}
+                textAlign={{smDown: "center"}}
+            >
+                {query ? (
+                    <Highlight query={query} styles={{color}}>
+                        {title}
+                    </Highlight>
+                ) : (
+                    <>{title}</>
+                )}
+            </Heading>
+        </>
+    );
+}
+
+function PromptShellBody(props: IPromptShellBodyProps) {
+    const {children} = props;
+
+    return (
+        <Flex justifyContent="center" alignSelf="stretch">
+            <Card.Root size="lg" flexGrow="1" maxInlineSize="prose">
+                <Card.Body gap="4">{children}</Card.Body>
+            </Card.Root>
+        </Flex>
+    );
+}
+
+function PromptShellContainer(props: IPromptShellContainerProps) {
+    const {children} = props;
+
+    return (
+        <VStack
+            gap="16"
+            align="center"
+            justify="center"
+            flexGrow="1"
+            padding={{lgTo2xl: "10", mdOnly: "5"}}
+            width="full"
+            background={{
+                //lgDown: "color-mix(in srgb, var(--chakra-colors-bg-inverted), transparent 95%)",
+                lgDown: "rgba(9, 9, 11, 0.15)",
+            }}
+        >
+            {children}
+
+            <Text
+                hideFrom="lg"
+                marginBlockStart="auto"
+                marginBlockEnd={{lgTo2xl: "-10", mdOnly: "-5"}}
+            >
+                {PACKAGE_NAME} v{PACKAGE_VERSION}
+            </Text>
+        </VStack>
+    );
+}
+
+function PromptShellSidebar(_props: IPromptShellSidebarProps) {
+    return (
+        <VStack
+            justify="center"
+            bg="bg.inverted"
+            color="fg.inverted"
+            hideBelow="lg"
+            padding="10"
+            maxInlineSize="96"
+            minInlineSize="96"
+        >
+            <Image
+                src="/images/logo.prompt.webp"
+                marginBlockStart="auto"
+                pointerEvents="none"
+            />
+
+            <Text marginBlockStart="auto" marginBlockEnd="-10">
+                {PACKAGE_NAME} v{PACKAGE_VERSION}
+            </Text>
+        </VStack>
+    );
+}
+
+function PromptShellRoot(props: IPromptShellRootProps) {
+    const {children} = props;
+
+    return (
+        <>
             <Image
                 src="/images/logo.prompt.webp"
                 hideFrom="lg"
@@ -55,68 +146,18 @@ export default function PromptShell(props: IPromptShellProps) {
             />
 
             <Flex align="stretch" width="dvw" height="dvh">
-                <VStack
-                    justify="center"
-                    bg="bg.inverted"
-                    color="fg.inverted"
-                    hideBelow="lg"
-                    padding="10"
-                    maxInlineSize="96"
-                    minInlineSize="96"
-                >
-                    <Image
-                        src="/images/logo.prompt.webp"
-                        marginBlockStart="auto"
-                        pointerEvents="none"
-                    />
-
-                    <Text marginBlockStart="auto" marginBlockEnd="-10">
-                        {PACKAGE_NAME} v{PACKAGE_VERSION}
-                    </Text>
-                </VStack>
-
-                <VStack
-                    gap="16"
-                    align="center"
-                    justify="center"
-                    flexGrow="1"
-                    padding={{lgTo2xl: "10", mdOnly: "5"}}
-                    width="full"
-                    background={{
-                        //lgDown: "color-mix(in srgb, var(--chakra-colors-bg-inverted), transparent 95%)",
-                        lgDown: "rgba(9, 9, 11, 0.15)",
-                    }}
-                >
-                    <Heading
-                        size="4xl"
-                        marginBlockStart={{lgDown: "auto"}}
-                        maxInlineSize={{smDown: "min"}}
-                        textAlign={{smDown: "center"}}
-                    >
-                        {query ? (
-                            <Highlight query={query} styles={{color}}>
-                                {title}
-                            </Highlight>
-                        ) : (
-                            <>{title}</>
-                        )}
-                    </Heading>
-
-                    <Flex justifyContent="center" alignSelf="stretch">
-                        <Card.Root size="lg" flexGrow="1" maxInlineSize="prose">
-                            <Card.Body gap="4">{children}</Card.Body>
-                        </Card.Root>
-                    </Flex>
-
-                    <Text
-                        hideFrom="lg"
-                        marginBlockStart="auto"
-                        marginBlockEnd={{lgTo2xl: "-10", mdOnly: "-5"}}
-                    >
-                        {PACKAGE_NAME} v{PACKAGE_VERSION}
-                    </Text>
-                </VStack>
+                {children}
             </Flex>
         </>
     );
 }
+
+const PromptShell = {
+    Body: PromptShellBody,
+    Container: PromptShellContainer,
+    Sidebar: PromptShellSidebar,
+    Root: PromptShellRoot,
+    Title: PromptShellTitle,
+};
+
+export default PromptShell;

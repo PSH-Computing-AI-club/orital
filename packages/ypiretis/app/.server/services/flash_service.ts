4a5b624820673ec221cbdc2e7a9fdc7ca4f9c13d
oridim <125223080+oridim@users.noreply.github.com>
2025-06-13 23:00:03 -0400

    feat: flash cookie service

---
+++ packages/ypiretis/app/.server/services/flash_service.ts
@@ -0,0 +1,22 @@
+import type {Session, SessionData} from "react-router";
+
+import {
+    commitSession as _commitSession,
+    destroySession as _destroySession,
+    getSession as _getSession,
+} from "../configuration/ephemeral_session";
+
+export type IFlashSession = Session<IFlashSessionData>;
+
+export interface IFlashSessionData extends SessionData {
+    readonly bearer?: string;
+}
+
+export const commitSession = (session: IFlashSession) =>
+    _commitSession(session);
+
+export const destroySession = (session: IFlashSession) =>
+    _destroySession(session);
+
+export const getSession = (cookieHeader: string) =>
+    _getSession(cookieHeader) as Promise<IFlashSession>;

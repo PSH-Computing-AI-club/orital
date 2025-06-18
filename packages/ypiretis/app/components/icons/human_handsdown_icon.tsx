8910d26b949732dbb11f01c0e5ab8e2f4c3b5b44
oridim <125223080+oridim@users.noreply.github.com>
2025-06-18 02:55:28 -0400

    feat: needed icons

---
+++ packages/ypiretis/app/components/icons/human_handsdown_icon.tsx
@@ -0,0 +1,10 @@
+import type {IIconProps} from "./icon";
+import IconRoot from "./icon";
+
+export default function HumanHandsdownIcon(props: IIconProps) {
+    return (
+        <IconRoot {...props}>
+            <path d="M10 2h4v4h-4V2zM7 7h10v2h-2v13h-2v-6h-2v6H9V9H7V7zm-2 4h2V9H5v2zm0 0v2H3v-2h2zm14 0h-2V9h2v2zm0 0h2v2h-2v-2z" />
+        </IconRoot>
+    );
+}

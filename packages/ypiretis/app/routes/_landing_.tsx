8765b24a0ca9ccee40245d371a5821bb4fc0c46b
oridim <125223080+oridim@users.noreply.github.com>
2025-06-20 05:39:16 -0400

    feat: proto landing navbar

--- packages/ypiretis/app/routes/_landing_.tsx
+++ packages/ypiretis/app/routes/_landing_.tsx
@@ -1,10 +1,44 @@
-import {Outlet} from "react-router";
+import {Container, HStack, Link} from "@chakra-ui/react";
+
+import {Link as RouterLink, Outlet} from "react-router";
 
 export default function LandingLayout() {
     return (
-        <div>
-            test!
+        <>
+            <Container
+                bg="bg"
+                borderBlockEndColor="border"
+                borderBlockEndStyle="solid"
+                borderBlockEndWidth="thin"
+                blockSize="16"
+                padding="4"
+            >
+                <HStack gap="4" alignItems="center" blockSize="full">
+                    <Link
+                        marginInlineStart="auto"
+                        _hover={{color: "cyan.solid"}}
+                        asChild
+                    >
+                        <RouterLink to="/news">News</RouterLink>
+                    </Link>
+                    <Link _hover={{color: "cyan.solid"}} asChild>
+                        <RouterLink to="/events">Events</RouterLink>
+                    </Link>
+                    C&AI
+                    <Link _hover={{color: "cyan.solid"}} asChild>
+                        <RouterLink to="/engage">Engage</RouterLink>
+                    </Link>
+                    <Link
+                        marginInlineEnd="auto"
+                        _hover={{color: "cyan.solid"}}
+                        asChild
+                    >
+                        <RouterLink to="/discord">Discord</RouterLink>
+                    </Link>
+                </HStack>
+            </Container>
+
             <Outlet />
-        </div>
+        </>
     );
 }

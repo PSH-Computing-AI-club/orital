a530878448aac1519a78811e2292a52cfec4f1b6
oridim <125223080+oridim@users.noreply.github.com>
2025-06-20 05:12:02 -0400

    refactor: changed to new layout routing

--- packages/ypiretis/app/routes/_index.tsx
+++
@@ -1,5 +0,0 @@
-import type {Route} from "./+types/_index";
-
-export default function Index(_props: Route.ComponentProps) {
-    return <h1>Hello world!</h1>;
-}
---
+++ packages/ypiretis/app/routes/_landing_._index.tsx
@@ -0,0 +1,5 @@
+import type {Route} from "./+types/_landing_._index";
+
+export default function LandingIndex(_props: Route.ComponentProps) {
+    return <h1>Hello world!</h1>;
+}

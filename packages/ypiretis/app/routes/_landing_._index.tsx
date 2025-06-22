09cd1f040e3b72349366027314c08aebdf4918c6
oridim <125223080+oridim@users.noreply.github.com>
2025-06-22 02:17:07 -0400

    feat: initial floating navbar

--- packages/ypiretis/app/routes/_landing_._index.tsx
+++ packages/ypiretis/app/routes/_landing_._index.tsx
@@ -1,5 +1,65 @@
 import type {Route} from "./+types/_landing_._index";
 
 export default function LandingIndex(_props: Route.ComponentProps) {
-    return <h1>Hello world!</h1>;
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+            <h1>Hello world!</h1>
+        </>
+    );
 }
--- packages/ypiretis/app/routes/_landing_.tsx
+++ packages/ypiretis/app/routes/_landing_.tsx
@@ -1,44 +1,60 @@
-import {Container, HStack, Link} from "@chakra-ui/react";
+import {Box, Container, HStack, Link} from "@chakra-ui/react";
 
 import {Link as RouterLink, Outlet} from "react-router";
 
 export default function LandingLayout() {
     return (
         <>
-            <Container
-                bg="bg"
-                borderBlockEndColor="border"
-                borderBlockEndStyle="solid"
-                borderBlockEndWidth="thin"
-                blockSize="16"
-                padding="4"
+            <Box
+                pos="sticky"
+                insetBlockStart="0"
+                blockSize="20"
+                paddingBlockStart="4"
+                paddingInline="8"
+                zIndex="2"
             >
-                <HStack gap="4" alignItems="center" blockSize="full">
-                    <Link
-                        marginInlineStart="auto"
-                        _hover={{color: "cyan.solid"}}
-                        asChild
-                    >
-                        <RouterLink to="/news">News</RouterLink>
-                    </Link>
-                    <Link _hover={{color: "cyan.solid"}} asChild>
-                        <RouterLink to="/events">Events</RouterLink>
-                    </Link>
-                    C&AI
-                    <Link _hover={{color: "cyan.solid"}} asChild>
-                        <RouterLink to="/engage">Engage</RouterLink>
-                    </Link>
-                    <Link
-                        marginInlineEnd="auto"
-                        _hover={{color: "cyan.solid"}}
-                        asChild
-                    >
-                        <RouterLink to="/discord">Discord</RouterLink>
-                    </Link>
-                </HStack>
-            </Container>
+                <Container blockSize="full">
+                    <Box
+                        position="absolute"
+                        inset="0"
+                        bg="bg"
+                        borderColor="border"
+                        borderStyle="solid"
+                        borderWidth="thin"
+                        zIndex="-1"
+                    />
 
-            <Outlet />
+                    <HStack gap="4" alignItems="center" blockSize="full">
+                        <Link
+                            marginInlineStart="auto"
+                            _hover={{color: "cyan.solid"}}
+                            asChild
+                        >
+                            <RouterLink to="/news">News</RouterLink>
+                        </Link>
+                        <Link _hover={{color: "cyan.solid"}} asChild>
+                            <RouterLink to="/events">Events</RouterLink>
+                        </Link>
+                        <strong>C&AI</strong>
+                        <Link _hover={{color: "cyan.solid"}} asChild>
+                            <RouterLink to="/engage">Engage</RouterLink>
+                        </Link>
+                        <Link
+                            marginInlineEnd="auto"
+                            _hover={{color: "cyan.solid"}}
+                            asChild
+                        >
+                            <RouterLink to="/discord">Discord</RouterLink>
+                        </Link>
+                    </HStack>
+                </Container>
+            </Box>
+
+            <Box paddingBlockEnd="4" zIndex="1">
+                <Container>
+                    <Outlet />
+                </Container>
+            </Box>
         </>
     );
 }

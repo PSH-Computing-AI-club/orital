bbccec1d342e3fee4e1a8d9c0b6d373b5ed52f14
oridim <125223080+oridim@users.noreply.github.com>
2025-06-25 12:24:17 -0400

    feat: proto landing design

--- packages/ypiretis/app/routes/_landing_._index.tsx
+++ packages/ypiretis/app/routes/_landing_._index.tsx
@@ -1,4 +1,21 @@
-import {Bleed, Box, Container} from "@chakra-ui/react";
+import {
+    Bleed,
+    Box,
+    Card,
+    Container,
+    Flex,
+    Heading,
+    HStack,
+    Image,
+    Link,
+    SimpleGrid,
+    Span,
+    Stack,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
+
+import {Link as RouterLink} from "react-router";
 
 import Background3DGrid from "~/components/frontpage/background_3d_grid";
 
@@ -23,67 +40,372 @@ export default function LandingIndex(_props: Route.ComponentProps) {
                 </Box>
             </Bleed>
 
-            <Box marginBlockEnd="4">
+            <Flex
+                alignItems="center"
+                bg="bg.inverted"
+                color="fg.inverted"
+                paddingBlock="8"
+                paddingInline={{base: "4", lgDown: "0"}}
+                minBlockSize="xl"
+            >
                 <Container>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
-                    <h1>Hello world!</h1>
+                    <Card.Root
+                        variant="subtle"
+                        gap={{base: "8", lgDown: "0"}}
+                        flexDirection={{base: "row", lgDown: "column"}}
+                        alignItems="center"
+                        justifyContent="space-between"
+                        bg="transparent"
+                        overflow="hidden"
+                    >
+                        <Image
+                            objectFit="cover"
+                            inlineSize="full"
+                            maxInlineSize="lg"
+                            src="/images/landing.psh.png"
+                        />
+
+                        <Card.Body gap="8">
+                            <Card.Title
+                                color="fg.inverted"
+                                fontSize={{base: "4xl", lgDown: "3xl"}}
+                                lineHeight="normal"
+                                textAlign={{base: "inherit", lgDown: "center"}}
+                            >
+                                We Are{" "}
+                                <Span whiteSpace="pre">
+                                    <Span whiteSpace="pre">Penn State</Span>{" "}
+                                    Harrisburg!
+                                </Span>
+                            </Card.Title>
+
+                            <Card.Description
+                                color="fg.subtle"
+                                fontSize={{base: "2xl", lgDown: "xl"}}
+                            >
+                                The Computing and AI Club provides a friendly
+                                atmosphere for all PSH students to network, hone
+                                their skills, and to have fun all things
+                                computing.
+                            </Card.Description>
+                        </Card.Body>
+                    </Card.Root>
                 </Container>
-            </Box>
+            </Flex>
+
+            <Flex
+                alignItems="center"
+                bg="bg.muted"
+                color="fg.inverted"
+                paddingBlock="8"
+                paddingInline={{base: "4", lgDown: "0"}}
+                minBlockSize="xl"
+            >
+                <Container>
+                    <Card.Root
+                        variant="subtle"
+                        gap="8"
+                        bg="transparent"
+                        overflow="hidden"
+                    >
+                        <Card.Body gap="4">
+                            <Card.Title
+                                fontSize={{base: "4xl", lgDown: "3xl"}}
+                                textAlign="center"
+                            >
+                                Events?
+                            </Card.Title>
+
+                            <Card.Description
+                                fontSize={{base: "2xl", lgDown: "xl"}}
+                                textAlign="center"
+                            >
+                                We got some of those.
+                            </Card.Description>
+
+                            <SimpleGrid
+                                columns={{base: 3, lgDown: 2, mdDown: 1}}
+                                gap="8"
+                                marginBlockStart="4"
+                            >
+                                <Card.Root>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            AI Injection Competition
+                                        </Card.Title>
+
+                                        <Card.Description>
+                                            November 20th, 2025
+                                        </Card.Description>
+
+                                        <Text marginBlockStart="4">
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magnam aliquam quaerat
+                                            voluptatem.
+                                        </Text>
+                                    </Card.Body>
+                                </Card.Root>
+
+                                <Card.Root>
+                                    <Card.Body>
+                                        <Card.Title>Event 2</Card.Title>
+
+                                        <Card.Description>
+                                            November ??th, 2025
+                                        </Card.Description>
+
+                                        <Text marginBlockStart="4">
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magnam aliquam quaerat
+                                            voluptatem.
+                                        </Text>
+                                    </Card.Body>
+                                </Card.Root>
+
+                                <Card.Root>
+                                    <Card.Body>
+                                        <Card.Title>Event 3</Card.Title>
+
+                                        <Card.Description>
+                                            November ??th, 2025
+                                        </Card.Description>
+
+                                        <Text marginBlockStart="4">
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magnam aliquam quaerat
+                                            voluptatem.
+                                        </Text>
+                                    </Card.Body>
+                                </Card.Root>
+                            </SimpleGrid>
+                        </Card.Body>
+                    </Card.Root>
+                </Container>
+            </Flex>
+
+            <Flex
+                alignItems="center"
+                bg="bg.inverted"
+                color="fg.inverted"
+                paddingBlock="8"
+                paddingInline={{base: "4", lgDown: "0"}}
+                minBlockSize="xl"
+            >
+                <Container>
+                    <Card.Root
+                        variant="subtle"
+                        gap={{base: "8", lgDown: "0"}}
+                        flexDirection={{base: "row", lgDown: "column-reverse"}}
+                        alignItems="center"
+                        justifyContent="space-between"
+                        bg="transparent"
+                        overflow="hidden"
+                    >
+                        <Card.Body gap="8">
+                            <Card.Title
+                                color="fg.inverted"
+                                fontSize={{base: "4xl", lgDown: "3xl"}}
+                                lineHeight="normal"
+                                textAlign={{base: "inherit", lgDown: "center"}}
+                            >
+                                Bi-Weekly Meetings
+                            </Card.Title>
+
+                            <Card.Description
+                                color="fg.subtle"
+                                fontSize={{base: "2xl", lgDown: "xl"}}
+                            >
+                                To promote club camaraderie, the Computing and
+                                AI Club hosts bi-weekly meetings every XYZday @
+                                common hour.
+                            </Card.Description>
+                        </Card.Body>
+
+                        <Image
+                            objectFit="cover"
+                            inlineSize="full"
+                            maxInlineSize="lg"
+                            src="/images/landing.calendar.png"
+                        />
+                    </Card.Root>
+                </Container>
+            </Flex>
+
+            <Flex
+                alignItems="center"
+                bg="bg.muted"
+                color="fg.inverted"
+                paddingBlock="8"
+                paddingInline={{base: "4", lgDown: "0"}}
+                minBlockSize="xl"
+            >
+                <Container>
+                    <Card.Root
+                        variant="subtle"
+                        gap="8"
+                        bg="transparent"
+                        overflow="hidden"
+                    >
+                        <Card.Body gap="4">
+                            <Card.Title
+                                fontSize={{base: "4xl", lgDown: "3xl"}}
+                                textAlign="center"
+                            >
+                                News?
+                            </Card.Title>
+
+                            <Card.Description
+                                fontSize={{base: "2xl", lgDown: "xl"}}
+                                textAlign="center"
+                            >
+                                Sure, we have our ramblings available.
+                            </Card.Description>
+
+                            <SimpleGrid
+                                columns={{base: 3, lgDown: 2, mdDown: 1}}
+                                gap="8"
+                                marginBlockStart="4"
+                            >
+                                <Card.Root>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            Dots and Boxes AI Competition Winner
+                                            Announced!
+                                        </Card.Title>
+
+                                        <Card.Description>
+                                            September 10th, 2025
+                                        </Card.Description>
+
+                                        <Text marginBlockStart="4">
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magnam aliquam quaerat
+                                            voluptatem.
+                                        </Text>
+                                    </Card.Body>
+                                </Card.Root>
+
+                                <Card.Root>
+                                    <Card.Body>
+                                        <Card.Title>News 2</Card.Title>
+
+                                        <Card.Description>
+                                            September ??th, 2025
+                                        </Card.Description>
+
+                                        <Text marginBlockStart="4">
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magnam aliquam quaerat
+                                            voluptatem.
+                                        </Text>
+                                    </Card.Body>
+                                </Card.Root>
+
+                                <Card.Root>
+                                    <Card.Body>
+                                        <Card.Title>News 3</Card.Title>
+
+                                        <Card.Description>
+                                            September ??th, 2025
+                                        </Card.Description>
+
+                                        <Text marginBlockStart="4">
+                                            Lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do
+                                            eiusmod tempor incididunt ut labore
+                                            et dolore magnam aliquam quaerat
+                                            voluptatem.
+                                        </Text>
+                                    </Card.Body>
+                                </Card.Root>
+                            </SimpleGrid>
+                        </Card.Body>
+                    </Card.Root>
+                </Container>
+            </Flex>
+
+            <Flex
+                bg="bg.inverted"
+                color="fg.inverted"
+                backgroundImage="url('/images/footer.border.png')"
+                backgroundSize="1024px"
+                backgroundRepeat="repeat-x"
+                paddingBlock="8"
+                paddingInline={{base: "4", lgDown: "0"}}
+                blockSize={{base: "sm", smDown: "lg"}}
+            >
+                <Container marginBlockStart="auto">
+                    <HStack gap="8" flexDirection={{base: "row"}}>
+                        <Heading size="7xl">
+                            C& <br />
+                            AI
+                        </Heading>
+
+                        <Box flexGrow="1" />
+
+                        <Stack
+                            gap="8"
+                            flexDirection={{base: "row", smDown: "column"}}
+                        >
+                            <VStack gap="2" alignItems="baseline">
+                                <Heading>Socials</Heading>
+                                <span>YouTube</span>
+                                <span>BlueSky</span>
+                                <span>???</span>
+                                <span>Profit!</span>
+                            </VStack>
+
+                            <VStack gap="2" alignItems="baseline">
+                                <Heading>Links</Heading>
+
+                                <Link
+                                    color="currentcolor"
+                                    _hover={{color: "cyan.solid"}}
+                                    asChild
+                                >
+                                    <RouterLink to="/news">News</RouterLink>
+                                </Link>
+
+                                <Link
+                                    color="currentcolor"
+                                    _hover={{color: "cyan.solid"}}
+                                    asChild
+                                >
+                                    <RouterLink to="/events">Events</RouterLink>
+                                </Link>
+
+                                <Link
+                                    color="currentcolor"
+                                    _hover={{color: "cyan.solid"}}
+                                    asChild
+                                >
+                                    <RouterLink to="/engage">Engage</RouterLink>
+                                </Link>
+
+                                <Link
+                                    color="currentcolor"
+                                    _hover={{color: "cyan.solid"}}
+                                    asChild
+                                >
+                                    <RouterLink to="/discord">
+                                        Discord
+                                    </RouterLink>
+                                </Link>
+                            </VStack>
+                        </Stack>
+                    </HStack>
+                </Container>
+            </Flex>
         </>
     );
 }

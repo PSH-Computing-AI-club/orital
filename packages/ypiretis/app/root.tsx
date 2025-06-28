bf42328be07e14498800affc07e87a1eef99ca64
oridim <125223080+oridim@users.noreply.github.com>
2025-06-28 14:18:29 -0400

    feat: favicon

--- packages/ypiretis/app/root.tsx
+++ packages/ypiretis/app/root.tsx
@@ -23,6 +23,24 @@ export function Layout(props: PropsWithChildren) {
                     content="width=device-width, initial-scale=1"
                 />
 
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon-96x96.png"
+                    sizes="96x96"
+                />
+
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+
+                <link rel="manifest" href="/site.webmanifest" />
+
                 <Meta />
                 <Links />
             </head>

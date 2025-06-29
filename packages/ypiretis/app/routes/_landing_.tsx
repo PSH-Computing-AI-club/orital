bd2c51551d4a247dbaf834e2997e0ad04b2b5855
oridim <125223080+oridim@users.noreply.github.com>
2025-06-29 10:49:41 -0400

    refactor: factored out sentinel functionality

--- packages/ypiretis/app/routes/_landing_.tsx
+++ packages/ypiretis/app/routes/_landing_.tsx
@@ -1,56 +1,15 @@
 import {Box, Container, Image, HStack, Link} from "@chakra-ui/react";
 
-import type {RefObject} from "react";
-import {useEffect, useRef, useState} from "react";
-
 import {Link as RouterLink, Outlet} from "react-router";
 
-function useNavbarSentinel(sentinelRef: RefObject<HTMLElement | null>) {
-    const [isObscured, setIsObscured] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (!sentinelRef.current) {
-            return;
-        }
-
-        const observer = new IntersectionObserver(
-            (entries) => {
-                const [firstEntry] = entries;
-                const {isIntersecting} = firstEntry;
-
-                setIsObscured(!isIntersecting);
-            },
-            {
-                root: null,
-                rootMargin: "0px",
-                threshold: 1,
-            },
-        );
-
-        observer.observe(sentinelRef.current);
-
-        return () => {
-            observer.disconnect();
-        };
-    }, [sentinelRef]);
-
-    return isObscured;
-}
+import useObscuredSentinel from "~/hooks/obscured_sentinel";
 
 export default function LandingLayout() {
-    const sentinelRef = useRef<HTMLElement | null>(null);
-    const isObscured = useNavbarSentinel(sentinelRef);
+    const {isObscured, ObscuredSentinel} = useObscuredSentinel();
 
     return (
         <>
-            <Box
-                ref={sentinelRef}
-                position="absolute"
-                insetBlockStart="0"
-                insetInlineStart="0"
-                blockSize="1px"
-                inlineSize="1px"
-            />
+            {ObscuredSentinel}
 
             <Box
                 pos="sticky"

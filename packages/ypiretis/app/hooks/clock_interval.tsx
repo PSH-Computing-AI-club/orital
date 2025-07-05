9a29324b675ca3eb79fc55badb51e02d0869e17a
oridim <125223080+oridim@users.noreply.github.com>
2025-07-05 19:11:19 -0400

    feat: clock-adjusted interval hook

---
+++ packages/ypiretis/app/hooks/clock_interval.tsx
@@ -0,0 +1,44 @@
+import {useEffect} from "react";
+
+export type IClockIntervalCallback = () => void;
+
+export interface IUseClockIntervalOptions {
+    readonly callback: IClockIntervalCallback;
+
+    readonly interval: number;
+}
+
+function calculateClockDelay(interval: number): number {
+    const now = Date.now();
+
+    return interval - (now % interval);
+}
+
+export default function useClockInterval(
+    options: IUseClockIntervalOptions,
+): void {
+    const {callback, interval} = options;
+
+    useEffect(() => {
+        let identifier: number;
+
+        function scheduleNextTimeout(): void {
+            const delay = calculateClockDelay(interval);
+
+            identifier = setTimeout(
+                () => {
+                    callback();
+                    scheduleNextTimeout();
+                },
+
+                delay,
+                // **HACK:** Node.JS's typing of `NodeJS.Timeout` override the browser's
+                // proper `number` return type.
+            ) as unknown as number;
+        }
+
+        scheduleNextTimeout();
+
+        return () => clearTimeout(identifier);
+    }, [callback, interval]);
+}

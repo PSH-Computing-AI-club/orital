40236b2ed030ad80cde991cd98943bd96bf68dab
oridim <125223080+oridim@users.noreply.github.com>
2025-07-11 06:43:08 -0400

    feat: snippet truncation func

---
+++ packages/ypiretis/app/.server/utils/string.ts
@@ -0,0 +1,25 @@
+const EXPRESSION_NEWLINE = /\n/g;
+
+export interface ITransformToSnippetOptions {
+    readonly limit: number;
+}
+
+export function transformTextToSnippet(
+    text: string,
+    options: ITransformToSnippetOptions,
+) {
+    const {limit} = options;
+
+    const trimmedText = text.trim().replace(EXPRESSION_NEWLINE, " ");
+
+    if (trimmedText.length <= limit) {
+        return trimmedText;
+    }
+
+    const lastSpaceIndex = trimmedText.substring(0, limit - 3).lastIndexOf(" ");
+    const truncationIndex = lastSpaceIndex > 0 ? lastSpaceIndex : limit - 3;
+
+    const truncatedText = trimmedText.substring(0, truncationIndex);
+
+    return `${truncatedText}...`;
+}

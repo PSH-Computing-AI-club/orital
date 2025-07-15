a3720d88a999d6339d4dbe021f8b4d5d087aaa1d
oridim <125223080+oridim@users.noreply.github.com>
2025-07-15 05:21:18 -0400

    feat: expose soft relations to Drizzle
    Drizzle's documentation, afaict, does not out right declare that the relations need to be ingested via the `schema` option...

--- packages/ypiretis/app/.server/configuration/database.ts
+++ packages/ypiretis/app/.server/configuration/database.ts
@@ -7,6 +7,7 @@ import {dirname} from "node:path";
 import {drizzle} from "drizzle-orm/bun-sqlite";
 
 import * as DATABASE_SCHEMA from "../database/tables";
+import * as DATABASE_RELATIONS from "../database/tables/relations";
 
 import ENVIRONMENT from "./environment";
 
@@ -28,7 +29,11 @@ CLIENT.run("PRAGMA journal_mode = WAL;");
 
 const DATABASE = drizzle({
     client: CLIENT,
-    schema: DATABASE_SCHEMA,
+
+    schema: {
+        ...DATABASE_SCHEMA,
+        ...DATABASE_RELATIONS,
+    },
 });
 
 export default DATABASE;

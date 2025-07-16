25dcf92034ae168d126d94d666943dce40f9b5b0
oridim <125223080+oridim@users.noreply.github.com>
2025-07-16 02:52:48 -0400

    fix: non-usage warning

--- packages/ypiretis/app/routes/rooms_.$roomID.presenter.polls.tsx
+++ packages/ypiretis/app/routes/rooms_.$roomID.presenter.polls.tsx
@@ -2,7 +2,7 @@ import AppShell from "~/components/shell/app_shell";
 
 import {Route} from "./+types/rooms_.$roomID.presenter.polls";
 
-export default function RoomsPresenterIndex(props: Route.ComponentProps) {
+export default function RoomsPresenterPolls(_props: Route.ComponentProps) {
     return (
         <AppShell.Container>
             Stuff will happun here at da polls!
--- packages/ypiretis/app/routes/rooms_.$roomID.presenter.settings.tsx
+++ packages/ypiretis/app/routes/rooms_.$roomID.presenter.settings.tsx
@@ -2,7 +2,7 @@ import AppShell from "~/components/shell/app_shell";
 
 import {Route} from "./+types/rooms_.$roomID.presenter.settings";
 
-export default function RoomsPresenterIndex(props: Route.ComponentProps) {
+export default function RoomsPresenterSettings(_props: Route.ComponentProps) {
     return (
         <AppShell.Container>
             Stuff will happun here at da settings!

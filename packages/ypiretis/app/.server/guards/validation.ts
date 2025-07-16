6d38d1815c13ef49796ab6b6221c4a89c49df999
oridim <125223080+oridim@users.noreply.github.com>
2025-07-16 02:24:07 -0400

    feat: support variant schemas in validation guard

--- packages/ypiretis/app/.server/guards/validation.ts
+++ packages/ypiretis/app/.server/guards/validation.ts
@@ -3,18 +3,24 @@ import {data} from "react-router";
 
 import type {
     ErrorMessage,
+    InferOutput,
     ObjectEntries,
     ObjectIssue,
     ObjectSchema,
+    VariantSchema,
 } from "valibot";
 import * as v from "valibot";
 
-export function validateParams<
-    T extends ObjectSchema<
-        ObjectEntries,
-        ErrorMessage<ObjectIssue> | undefined
-    >,
->(
+export type IObjectSchema = ObjectSchema<
+    ObjectEntries,
+    ErrorMessage<ObjectIssue> | undefined
+>;
+
+export type IObjectLikeSchema =
+    | IObjectSchema
+    | VariantSchema<string, IObjectSchema[], undefined>;
+
+export function validateParams<T extends IObjectLikeSchema>(
     schema: T,
     requestArgs: ActionFunctionArgs | LoaderFunctionArgs,
     errorData: unknown = "Bad Request",

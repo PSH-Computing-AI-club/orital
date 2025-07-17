bb893d6fc545295ce47302d794ba1ce681b4dc4f
oridim <125223080+oridim@users.noreply.github.com>
2025-07-17 01:56:55 -0400

    feat: query paginated articles at admin news index

---
+++ packages/ypiretis/app/routes/admin_.news.($page).tsx
@@ -0,0 +1,104 @@
+import Layout from "~/components/controlpanel/layout";
+
+import {data} from "react-router";
+
+import * as v from "valibot";
+
+import {findAll} from "~/.server/services/articles_service";
+
+import {FORMAT_DETAIL, formatZonedDateTime} from "~/.server/utils/locale";
+import {SYSTEM_TIMEZONE} from "~/.server/utils/temporal";
+
+import {validateParams} from "~/guards/validation";
+
+import {Route} from "./+types/admin_.news.($page)";
+
+const ARTICLES_PER_PAGE = 25;
+
+const LOADER_PARAMS_SCHEMA = v.object({
+    page: v.optional(
+        v.pipe(
+            v.string(),
+            v.transform((value) => Number(value)),
+            v.number(),
+        ),
+    ),
+});
+
+export async function loader(loaderArgs: Route.LoaderArgs) {
+    const {page = 1} = validateParams(LOADER_PARAMS_SCHEMA, loaderArgs);
+
+    const {articles, pagination} = await findAll({
+        pagination: {
+            page,
+
+            limit: ARTICLES_PER_PAGE,
+        },
+    });
+
+    const {pages} = pagination;
+
+    if (page > pages) {
+        throw data("Not Found", {
+            status: 404,
+        });
+    }
+
+    const mappedArticles = await Promise.all(
+        articles.map(async (article) => {
+            const {articleID, createdAt, publishedAt, title, updatedAt} =
+                article;
+
+            const zonedCreatedAt =
+                createdAt.toZonedDateTimeISO(SYSTEM_TIMEZONE);
+
+            const zonedUpdatedAt = publishedAt
+                ? updatedAt.toZonedDateTimeISO(SYSTEM_TIMEZONE)
+                : null;
+
+            const zonedPublishedAt = publishedAt
+                ? publishedAt.toZonedDateTimeISO(SYSTEM_TIMEZONE)
+                : null;
+
+            const createdAtText = formatZonedDateTime(zonedCreatedAt, {
+                detail: FORMAT_DETAIL.short,
+            });
+
+            const publishedAtText = zonedPublishedAt
+                ? formatZonedDateTime(zonedPublishedAt, {
+                      detail: FORMAT_DETAIL.short,
+                  })
+                : null;
+
+            const updatedAtText = zonedUpdatedAt
+                ? formatZonedDateTime(zonedUpdatedAt, {
+                      detail: FORMAT_DETAIL.short,
+                  })
+                : null;
+
+            return {
+                articleID,
+                createdAtText,
+                publishedAtText,
+                updatedAtText,
+                title,
+            };
+        }),
+    );
+
+    return {
+        articles: mappedArticles,
+        pagination,
+    };
+}
+
+export default function AdminNews(props: Route.ComponentProps) {
+    const {loaderData} = props;
+    const {articles} = loaderData;
+
+    return (
+        <Layout.FixedContainer>
+            News index unda construction
+        </Layout.FixedContainer>
+    );
+}

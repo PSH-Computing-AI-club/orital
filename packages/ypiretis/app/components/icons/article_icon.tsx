ad22e1b054ef4fc4110135e74c4d0dc65c54615e
oridim <125223080+oridim@users.noreply.github.com>
2025-07-17 00:33:54 -0400

    feat: needed icons

---
+++ packages/ypiretis/app/components/icons/article_icon.tsx
@@ -0,0 +1,10 @@
+import type {IIconProps} from "./icon";
+import IconRoot from "./icon";
+
+export default function ArticleIcon(props: IIconProps) {
+    return (
+        <IconRoot {...props}>
+            <path d="M5 3H3v18h18V3H5zm14 2v14H5V5h14zm-2 2H7v2h10V7zM7 11h10v2H7v-2zm7 4H7v2h7v-2z" />
+        </IconRoot>
+    );
+}

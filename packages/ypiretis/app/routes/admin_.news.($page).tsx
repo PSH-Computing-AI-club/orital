74071aba652d4ffd7ec953180dcfc824f76083ed
oridim <125223080+oridim@users.noreply.github.com>
2025-07-21 15:23:51 -0400

    feat: initial table design for admin news index

--- packages/ypiretis/app/routes/admin_.news.($page).tsx
+++ packages/ypiretis/app/routes/admin_.news.($page).tsx
@@ -1,3 +1,5 @@
+import {Code, Table} from "@chakra-ui/react";
+
 import {data} from "react-router";
 
 import * as v from "valibot";
@@ -109,12 +111,102 @@ export async function loader(loaderArgs: Route.LoaderArgs) {
 
 export default function AdminNews(props: Route.ComponentProps) {
     const {loaderData} = props;
-    const {articles} = loaderData;
+    const {articles, pagination} = loaderData;
 
     return (
         <Layout.FixedContainer>
             <Title.Text title="News Articles" />
-            News index unda construction
+
+            <Table.ScrollArea
+                borderColor="border"
+                borderStyle="solid"
+                borderWidth="thin"
+            >
+                <Table.Root showColumnBorder stickyHeader>
+                    <Table.Header>
+                        <Table.Row
+                            css={{
+                                _after: {
+                                    display: "block",
+                                    content: '""',
+
+                                    blockSize: "1px",
+                                    inlineSize: "full",
+
+                                    position: "absolute",
+                                    insetBlockEnd: "0",
+                                    insetInlineStart: "0",
+
+                                    bg: "border",
+                                },
+                            }}
+                        >
+                            <Table.ColumnHeader>Article ID</Table.ColumnHeader>
+                            <Table.ColumnHeader>Title</Table.ColumnHeader>
+                            <Table.ColumnHeader>Poster</Table.ColumnHeader>
+                            <Table.ColumnHeader>State</Table.ColumnHeader>
+
+                            <Table.ColumnHeader>
+                                Created Date
+                            </Table.ColumnHeader>
+
+                            <Table.ColumnHeader>
+                                Published Date
+                            </Table.ColumnHeader>
+
+                            <Table.ColumnHeader>
+                                Updated Date
+                            </Table.ColumnHeader>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {articles.map((article, _index) => {
+                            const {
+                                articleID,
+                                createdAtText,
+                                poster,
+                                publishedAtText,
+                                state,
+                                title,
+                                updatedAtText,
+                            } = article;
+
+                            const {firstName, lastName} = poster;
+
+                            return (
+                                <Table.Row key={articleID}>
+                                    <Table.Cell>
+                                        <Code>{articleID}</Code>
+                                    </Table.Cell>
+
+                                    <Table.Cell>{title}</Table.Cell>
+
+                                    <Table.Cell>
+                                        {lastName}, {firstName}
+                                    </Table.Cell>
+
+                                    <Table.Cell>
+                                        {state === "STATE_DRAFT"
+                                            ? "draft"
+                                            : "published"}
+                                    </Table.Cell>
+
+                                    <Table.Cell>{createdAtText}</Table.Cell>
+
+                                    <Table.Cell>
+                                        {publishedAtText ?? "-"}
+                                    </Table.Cell>
+
+                                    <Table.Cell>
+                                        {updatedAtText ?? "-"}
+                                    </Table.Cell>
+                                </Table.Row>
+                            );
+                        })}
+                    </Table.Body>
+                </Table.Root>
+            </Table.ScrollArea>
         </Layout.FixedContainer>
     );
 }

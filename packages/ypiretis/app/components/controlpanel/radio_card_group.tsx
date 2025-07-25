6cdda3c6b22fc5aa1bd0a9cac138edda1a4c0dc0
oridim <125223080+oridim@users.noreply.github.com>
2025-07-25 11:02:46 -0400

    feat: radio card group component
    This is to keep a consistent style and usage pattern between routes.

---
+++ packages/ypiretis/app/components/controlpanel/radio_card_group.tsx
@@ -0,0 +1,71 @@
+import type {RadioCardItemProps, RadioCardRootProps} from "@chakra-ui/react";
+import {HStack, Icon, RadioCard, useRadioCardContext} from "@chakra-ui/react";
+
+import type {ReactElement, ReactNode} from "react";
+
+export interface IRadioCardGroupOptionProps
+    extends Omit<RadioCardItemProps, "children"> {
+    readonly icon?: ReactElement;
+
+    readonly label: ReactNode;
+
+    readonly value: string;
+}
+
+export interface IRadioCardGroupRootProps extends RadioCardRootProps {}
+
+function RadioCardGroupOption(props: IRadioCardGroupOptionProps) {
+    const {icon, label, value, ...rest} = props;
+
+    const {getRootProps, value: selectedValue} = useRadioCardContext();
+    const disabled = getRootProps()["aria-disabled"];
+
+    const isActive = selectedValue === value;
+    const canSetCursor = !disabled && !isActive;
+
+    const cursor = isActive ? "default" : "pointer";
+
+    return (
+        <RadioCard.Item
+            value={value}
+            cursor={canSetCursor ? cursor : undefined}
+            {...rest}
+        >
+            <RadioCard.ItemHiddenInput />
+
+            <RadioCard.ItemControl>
+                {icon ? <Icon fontSize="2xl">{icon}</Icon> : undefined}
+
+                <RadioCard.ItemText flexGrow="unset">
+                    {label}
+                </RadioCard.ItemText>
+            </RadioCard.ItemControl>
+        </RadioCard.Item>
+    );
+}
+
+function RadioCardGroupRoot(props: IRadioCardGroupRootProps) {
+    const {children, ...rest} = props;
+
+    return (
+        <RadioCard.Root
+            variant="solid"
+            orientation="vertical"
+            align="center"
+            justify="center"
+            alignSelf="stretch"
+            {...rest}
+        >
+            <HStack flexGrow="1" alignItems="stretch" justifyContent="stretch">
+                {children}
+            </HStack>
+        </RadioCard.Root>
+    );
+}
+
+const RadioCardGroup = {
+    Option: RadioCardGroupOption,
+    Root: RadioCardGroupRoot,
+};
+
+export default RadioCardGroup;

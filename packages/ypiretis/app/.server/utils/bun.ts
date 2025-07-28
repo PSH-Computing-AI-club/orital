7b88c3bbc9fcbb43de80a89b5fb7870c44c83481
oridim <125223080+oridim@users.noreply.github.com>
2025-07-28 14:37:59 -0400

    feat: add move helper with a copy fallback
    We cannot rename across devices / partitions. So, we need to fallback to copy operations in that scenario.

--- packages/ypiretis/app/.server/utils/bun.ts
+++ packages/ypiretis/app/.server/utils/bun.ts
@@ -10,3 +10,32 @@ export function isBunFile(value: unknown): value is Bun.BunFile {
         (value as any).name.length > 0
     );
 }
+
+export async function moveFile(
+    sourceFilePath: string | Bun.BunFile,
+    destinationFilePath: string,
+): Promise<Bun.BunFile> {
+    const sourceFile =
+        typeof sourceFilePath === "string"
+            ? Bun.file(sourceFilePath)
+            : sourceFilePath;
+
+    const destinationFile = Bun.file(destinationFilePath);
+
+    try {
+        await rename(sourceFile.name!, destinationFilePath);
+    } catch (error) {
+        if (
+            error instanceof Error &&
+            "code" in error &&
+            error.code === "EXDEV"
+        ) {
+            await Bun.write(destinationFile, sourceFile);
+            await sourceFile.unlink();
+        } else {
+            throw error;
+        }
+    }
+
+    return destinationFile;
+}

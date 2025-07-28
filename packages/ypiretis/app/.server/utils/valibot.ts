a0b8206b551c603a67155eb5cab58457483812c4
oridim <125223080+oridim@users.noreply.github.com>
2025-07-28 13:36:35 -0400

    feat: valibot support for handling file and directory paths

--- packages/ypiretis/app/.server/utils/valibot.ts
+++ packages/ypiretis/app/.server/utils/valibot.ts
@@ -1,6 +1,62 @@
+import {mkdir} from "node:fs/promises";
+import {dirname, isAbsolute, resolve} from "node:path";
+import {cwd} from "node:process";
+import {platform} from "node:os";
+
 import {CronPattern} from "croner";
 
 import * as v from "valibot";
+
+const IS_WINDOWS = platform() === "win32";
+
+const PROCESS_CWD = cwd();
+
+const EXPRESSION_NTFS_ABSOLUTE_PATH =
+    /^(?:[a-zA-Z]:\\|\\\\[a-zA-Z0-9_.-]+\\[a-zA-Z0-9_.-]+)\\?(?:[^<>:"/\\|?*]+\\?)*$/;
+
+const EXPRESSION_NTFS_RELATIVE_PATH =
+    /^(?![a-zA-Z]:\\)(?!\\\\)(?!\\)(?!.*[:<>"|?*]).*$/;
+
+const EXPRESSION_POSIX_ABSOLUTE_PATH = /^\/(?:[^/]+\/)*[^/]*$/;
+
+const EXPRESSION_POSIX_RELATIVE_PATH = /^[^/]+(?:\/[^/]+)*\/?$/;
+
+const resolvePath = v.transform((path: string) => {
+    return isAbsolute(path) ? path : resolve(PROCESS_CWD, path);
+});
+
+export const ntfsAbsolutePath = v.regex(
+    EXPRESSION_NTFS_ABSOLUTE_PATH,
+    "Invalid absolute Windows file path format.",
+);
+
+export const ntfsRelativePath = v.regex(
+    EXPRESSION_NTFS_RELATIVE_PATH,
+    "Invalid relative Windows file path format.",
+);
+
+export const posixAbsolutePath = v.regex(
+    EXPRESSION_POSIX_ABSOLUTE_PATH,
+    "Invalid absolute POSIX file path format.",
+);
+
+export const posixRelativePath = v.regex(
+    EXPRESSION_POSIX_RELATIVE_PATH,
+    "Invalid relative POSIX file path format.",
+);
+
+export const systemAbsolutePath = IS_WINDOWS
+    ? ntfsAbsolutePath
+    : posixAbsolutePath;
+
+export const systemRelativePath = IS_WINDOWS
+    ? ntfsRelativePath
+    : posixRelativePath;
+
+export const systemPath = v.union([
+    v.pipe(v.string(), systemAbsolutePath),
+    v.pipe(v.string(), systemRelativePath),
+]);
 
 export const cronExpression = v.pipe(
     v.string(),
@@ -13,4 +69,30 @@ export const cronExpression = v.pipe(
 
         return true;
     }, "Invalid cron expression format."),
+);
+
+export const directoryPath = v.pipeAsync(
+    systemPath,
+    resolvePath,
+    v.transformAsync(async (resolvedDirectoryPath) => {
+        await mkdir(resolvedDirectoryPath, {
+            recursive: true,
+        });
+
+        return resolvedDirectoryPath;
+    }),
+);
+
+export const filePath = v.pipeAsync(
+    systemPath,
+    resolvePath,
+    v.transformAsync(async (resolvedFilePath) => {
+        const directoryPath = dirname(resolvedFilePath);
+
+        await mkdir(directoryPath, {
+            recursive: true,
+        });
+
+        return resolvedFilePath;
+    }),
 );

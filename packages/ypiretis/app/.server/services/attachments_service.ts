18f01d308a498350ead2b0fb0a677cf1862cd973
oridim <125223080+oridim@users.noreply.github.com>
2025-07-29 22:03:23 -0400

    feat: placeholder service factory
    Adjusted the `handleAttachment` function to accept a user since it will be calling into ``handleFile` from the uploads service. Also, changed `handleAttachment` to accept a `Bun.BunFile` instead of a plain `File` for the same reason.

--- packages/ypiretis/app/.server/services/attachments_service.ts
+++ packages/ypiretis/app/.server/services/attachments_service.ts
@@ -1,9 +1,30 @@
-import type {ISelectAttachment} from "../database/tables/attachments_table";
+import type {
+    IAttachmentsTable,
+    ISelectAttachment,
+} from "../database/tables/attachments_table";
+
+import type {IUser} from "./users_service";
 
 export interface IAttachmentsService {
     deleteAttachment(targetID: number, uploadID: number): Promise<void>;
 
     findAllAttachments(targetID: number): Promise<ISelectAttachment[]>;
 
-    handleAttachment(targetID: number, file: File): Promise<ISelectAttachment>;
+    handleAttachment(
+        targetID: number,
+        user: IUser,
+        file: Bun.BunFile,
+    ): Promise<ISelectAttachment>;
+}
+
+export function makeAttachmentsService<A extends IAttachmentsTable>(
+    attachmentsTable: A,
+): IAttachmentsService {
+    return {
+        deleteAttachment(targetID, uploadID) {},
+
+        findAllAttachments(targetID) {},
+
+        async handleAttachment(targetID, user, file) {},
+    };
 }

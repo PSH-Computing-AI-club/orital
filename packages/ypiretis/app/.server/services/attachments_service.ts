a75e6f396bbd8a3ae54f89d5f8c28388fd6d3963
oridim <125223080+oridim@users.noreply.github.com>
2025-07-29 21:53:04 -0400

    fix: used wrong type

--- packages/ypiretis/app/.server/services/attachments_service.ts
+++ packages/ypiretis/app/.server/services/attachments_service.ts
@@ -1,12 +1,9 @@
-import type {
-    IInsertAttachment,
-    ISelectAttachment,
-} from "../database/tables/attachments_table";
+import type {ISelectAttachment} from "../database/tables/attachments_table";
 
 export interface IAttachmentsService {
     deleteAttachment(targetID: number, uploadID: number): Promise<void>;
 
-    findAllAttachments(targetID: number): Promise<IInsertAttachment[]>;
+    findAllAttachments(targetID: number): Promise<ISelectAttachment[]>;
 
     handleAttachment(targetID: number, file: File): Promise<ISelectAttachment>;
 }

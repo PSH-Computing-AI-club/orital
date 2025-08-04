d9612190cff554ffe08e5a6a911240757000c461
oridim <125223080+oridim@users.noreply.github.com>
2025-08-03 21:06:51 -0400

    feat: needed icon

---
+++ packages/ypiretis/app/components/icons/upload_icon.tsx
@@ -0,0 +1,9 @@
+import {makeIconComponent} from "./icon";
+
+const UploadIcon = makeIconComponent({
+    icon: (
+        <path d="M11 5V3h2v2h2v2h2v2h-2V7h-2v10h-2V7H9v2H7V7h2V5h2zM3 15v6h18v-6h-2v4H5v-4H3z" />
+    ),
+});
+
+export default UploadIcon;

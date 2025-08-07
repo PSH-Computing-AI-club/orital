2dd4e18f297b0b2438c994a5c8c0206bef0cd55b
oridim <125223080+oridim@users.noreply.github.com>
2025-08-07 02:32:00 -0400

    feat: added needed icons

---
+++ packages/ypiretis/app/components/icons/check_box_icon.tsx
@@ -0,0 +1,9 @@
+import {makeIconComponent} from "./icon";
+
+const CheckBoxIcon = makeIconComponent({
+    icon: (
+        <path d="M5 3H3v18h18V3H5zm0 2h14v14H5V5zm4 7H7v2h2v2h2v-2h2v-2h2v-2h2V8h-2v2h-2v2h-2v2H9v-2z" />
+    ),
+});
+
+export default CheckBoxIcon;

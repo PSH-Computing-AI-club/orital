932cccda8ec6ebca7397d90a6b80fcabd28f5d15
oridim <125223080+oridim@users.noreply.github.com>
2025-08-09 03:19:27 -0400

    feat: added Markdown service formatting wrapper around prettier

--- packages/ypiretis/app/.server/services/markdown/service.ts
+++ packages/ypiretis/app/.server/services/markdown/service.ts
@@ -1,7 +1,28 @@
+import type {Options} from "prettier";
+import {format} from "prettier";
+
 import PLAINTEXT_MARKED from "./configurations/plaintext_marked";
 import WEB_MARKED from "./configurations/web_marked";
 
 import santizeHTML from "./sanitize";
+
+const PRETTIER_OPTIONS = {
+    parser: "markdown",
+
+    arrowParens: "always",
+    bracketSpacing: false,
+    endOfLine: "lf",
+    printWidth: 80,
+    proseWrap: "preserve",
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: false,
+} satisfies Options;
+
+export function formatMarkdown(text: string): Promise<string> {
+    return format(text, PRETTIER_OPTIONS);
+}
 
 export async function renderMarkdownForWeb(text: string): Promise<string> {
     const htmlOutput = await WEB_MARKED.parse(text);

37b52187c62e530dd726e9781581d8e728fbb190
oridim <125223080+oridim@users.noreply.github.com>
2025-08-10 23:26:57 -0400

    feat: added defaults for non-sensitive environment variables to improve system admin experience

--- packages/ypiretis/app/.server/configuration/environment.ts
+++ packages/ypiretis/app/.server/configuration/environment.ts
@@ -45,24 +45,30 @@ export const NODE_ENVIRONMENT_MODES = {
 } as const;
 
 export const ENVIRONMENT_SCHEMA = v.object({
-    NODE_ENV: v.enum(NODE_ENVIRONMENT_MODES),
+    NODE_ENV: v.optional(
+        v.enum(NODE_ENVIRONMENT_MODES),
+        NODE_ENVIRONMENT_MODES.development,
+    ),
 
-    SERVER_LOGGING_LEVEL: v.enum(LOGGING_LEVELS),
+    SERVER_LOGGING_LEVEL: v.optional(
+        v.enum(LOGGING_LEVELS),
+        LOGGING_LEVELS.info,
+    ),
 
-    SERVER_HOST: hostname,
-    SERVER_PORT: number,
+    SERVER_HOST: v.optional(hostname, "localhost"),
+    SERVER_PORT: v.optional(number, "3333"),
 
     SECRET_KEY: cryptographicKey,
     SECRET_SALT: cryptographicKey,
 
-    DATABASE_FILE_PATH: filePath,
+    DATABASE_FILE_PATH: v.optional(filePath, "./tmp/userdata.sqlite"),
 
-    UPLOADS_DIRECTORY_PATH: directoryPath,
-    UPLOADS_MAX_FILE_SIZE: byteSize,
+    UPLOADS_DIRECTORY_PATH: v.optional(directoryPath, "./tmp/uploads"),
+    UPLOADS_MAX_FILE_SIZE: v.optional(byteSize, "5MB"),
 
-    ARTICLES_ATTACHMENTS_MAX_FILE_SIZE: byteSize,
+    ARTICLES_ATTACHMENTS_MAX_FILE_SIZE: v.optional(byteSize, "5MB"),
 
-    EVENTS_ATTACHMENTS_MAX_FILE_SIZE: byteSize,
+    EVENTS_ATTACHMENTS_MAX_FILE_SIZE: v.optional(byteSize, "5MB"),
 
     SMTP_HOST: hostname,
     SMTP_PORT: number,
@@ -70,29 +76,35 @@ export const ENVIRONMENT_SCHEMA = v.object({
     SMTP_EMAIL: email,
     SMTP_PASSWORD: v.string(),
 
-    APP_NAME: v.pipe(v.string(), v.nonEmpty(), v.maxLength(32)),
-    APP_URL: url,
+    APP_NAME: v.optional(
+        v.pipe(v.string(), v.nonEmpty(), v.maxLength(32)),
+        "Orital Ypiretis",
+    ),
+    APP_URL: v.optional(url, "http://localhost:3333"),
 
     ACCOUNT_PROVIDER_DOMAIN: domain,
     ACCOUNT_PROVIDER_NAME: v.pipe(v.string(), v.nonEmpty(), v.maxLength(64)),
 
     ACCOUNT_ADMIN_IDENTIFIERS: identifierList,
 
-    SESSION_EPHEMERAL_TTL: duration,
-    SESSION_PERSISTENT_TTL: duration,
+    SESSION_EPHEMERAL_TTL: v.optional(duration, "PT2H"),
+    SESSION_PERSISTENT_TTL: v.optional(duration, "P30D"),
 
-    TOKEN_CALLBACK_TTL: duration,
-    TOKEN_CONSENT_TTL: duration,
-    TOKEN_GRANT_TTL: duration,
+    TOKEN_CALLBACK_TTL: v.optional(duration, "PT5M"),
+    TOKEN_CONSENT_TTL: v.optional(duration, "PT5M"),
+    TOKEN_GRANT_TTL: v.optional(duration, "PT5M"),
 
-    ROOMS_DISCONNECT_TTL: duration,
-    ROOMS_LIFETIME_TTL: duration,
+    ROOMS_DISCONNECT_TTL: v.optional(duration, "PT1H"),
+    ROOMS_LIFETIME_TTL: v.optional(duration, "PT24H"),
 
-    CRONJOB_ROOMS_DISCONNECT_CLEANUP: cronExpression,
-    CRONJOB_ROOMS_LIFETIME_CLEANUP: cronExpression,
-    CRONJOB_TOKENS_CLEANUP: cronExpression,
+    CRONJOB_ROOMS_DISCONNECT_CLEANUP: v.optional(
+        cronExpression,
+        "*/25 * * * *",
+    ),
+    CRONJOB_ROOMS_LIFETIME_CLEANUP: v.optional(cronExpression, "0 */6 * * *"),
+    CRONJOB_TOKENS_CLEANUP: v.optional(cronExpression, "*/10 * * * *"),
 
-    QUEUE_EMAILS_MAX: number,
+    QUEUE_EMAILS_MAX: v.optional(number, "10"),
 
     DISCORD_INVITE_CODE: alphanumerical,
 

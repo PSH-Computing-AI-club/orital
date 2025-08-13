44d1929117f0092cbd86159cda645dbb5919630a
oridim <125223080+oridim@users.noreply.github.com>
2025-08-13 04:20:05 -0400

    fix: Bun does not have `navigator.language` set in its runtime

--- packages/ypiretis/app/utils/navigator.ts
+++ packages/ypiretis/app/utils/navigator.ts
@@ -1,7 +1,7 @@
 // **IMPORTANT:** Do **NOT** use absolute imports in this module. It is
 // imported by server-only modules.
 
-export const NAVIGATOR_LANGUAGE = navigator.language;
+export const {language: NAVIGATOR_LANGUAGE = "en-US"} = navigator;
 
 export const {timeZone: NAVIGATOR_TIMEZONE} =
     Intl.DateTimeFormat().resolvedOptions();

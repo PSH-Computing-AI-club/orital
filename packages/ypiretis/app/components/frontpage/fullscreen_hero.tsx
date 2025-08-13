051e54f8e22b4971ad8e51e1e7b793766a2578e0
oridim <125223080+oridim@users.noreply.github.com>
2025-08-13 02:48:41 -0400

    fix: use full viewport height w/o accounting for any floating / dynamic omnibar
    I really do not like this but it prevents layout shifting whenever the user scrolls up on their phone and the omnibar goes out of view.

--- packages/ypiretis/app/components/frontpage/fullscreen_hero.tsx
+++ packages/ypiretis/app/components/frontpage/fullscreen_hero.tsx
@@ -14,7 +14,7 @@ function FullscreenHeroRoot(props: IFullscreenHeroRootProps) {
             bg="bg.inverted"
             color="fg.inverted"
             paddingBlockStart="calc(var(--chakra-sizes-16) + var(--chakra-spacing-8) + (var(--chakra-spacing-2) * 2) + 2px)"
-            blockSize="dvh"
+            blockSize="vh"
         >
             {children}
         </Box>

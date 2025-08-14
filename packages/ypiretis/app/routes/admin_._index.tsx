4a6bbc755bfff581601b3589420ecb9301d6dfae
oridim <125223080+oridim@users.noreply.github.com>
2025-08-14 02:09:40 -0400

    feat: added empty state for admin dashboard

--- packages/ypiretis/app/routes/admin_._index.tsx
+++ packages/ypiretis/app/routes/admin_._index.tsx
@@ -1,3 +1,6 @@
+import DownasaurIcon from "~/components/icons/downasaur_icon";
+
+import EmptyState from "~/components/controlpanel/empty_state";
 import Layout from "~/components/controlpanel/layout";
 
 import {Route} from "./+types/admin_._index";
@@ -5,7 +8,21 @@ import {Route} from "./+types/admin_._index";
 export default function AdminIndex(_props: Route.ComponentProps) {
     return (
         <Layout.FixedContainer>
-            Admin index unda construction
+            <EmptyState.Root>
+                <EmptyState.Container>
+                    <EmptyState.Icon>
+                        <DownasaurIcon />
+                    </EmptyState.Icon>
+
+                    <EmptyState.Body>
+                        <EmptyState.Title>To be implemented</EmptyState.Title>
+
+                        <EmptyState.Description>
+                            This feature has not yet been implemented.
+                        </EmptyState.Description>
+                    </EmptyState.Body>
+                </EmptyState.Container>
+            </EmptyState.Root>
         </Layout.FixedContainer>
     );
 }

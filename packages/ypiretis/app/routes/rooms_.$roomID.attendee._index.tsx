80b26011a0056033612cdd5f0197d046375db128
oridim <125223080+oridim@users.noreply.github.com>
2025-08-17 03:16:25 -0400

    feat: set page title for attendee portal

--- packages/ypiretis/app/routes/rooms_.$roomID.attendee._index.tsx
+++ packages/ypiretis/app/routes/rooms_.$roomID.attendee._index.tsx
@@ -1,3 +1,5 @@
+import Title from "~/components/common/title";
+
 import EmptyState from "~/components/controlpanel/empty_state";
 import Layout from "~/components/controlpanel/layout";
 
@@ -9,37 +11,42 @@ import {useAttendeeContext} from "~/state/attendee";
 import {Route} from "./+types/rooms_.$roomID.presenter.settings";
 
 export default function RoomsAttendeeIndex(_props: Route.ComponentProps) {
-    const {state} = useAttendeeContext();
+    const {state, room} = useAttendeeContext();
+    const {title} = room;
 
     const isAwaitingApproval = state === "STATE_AWAITING";
 
     return (
-        <Layout.FixedContainer>
-            <EmptyState.Root>
-                <EmptyState.Container>
-                    <EmptyState.Icon>
-                        {isAwaitingApproval ? (
-                            <ClipboardIcon />
-                        ) : (
-                            <MessageClockIcon />
-                        )}
-                    </EmptyState.Icon>
+        <>
+            <Title title={title || "Awaiting approval."} />
 
-                    <EmptyState.Body>
-                        <EmptyState.Title>
-                            {isAwaitingApproval
-                                ? "Awaiting approval"
-                                : "Waiting on presenter"}
-                        </EmptyState.Title>
+            <Layout.FixedContainer>
+                <EmptyState.Root>
+                    <EmptyState.Container>
+                        <EmptyState.Icon>
+                            {isAwaitingApproval ? (
+                                <ClipboardIcon />
+                            ) : (
+                                <MessageClockIcon />
+                            )}
+                        </EmptyState.Icon>
 
-                        <EmptyState.Description>
-                            {isAwaitingApproval
-                                ? "The presenter needs to approval you joining the room."
-                                : "The presenter has not given you any actions to do yet."}
-                        </EmptyState.Description>
-                    </EmptyState.Body>
-                </EmptyState.Container>
-            </EmptyState.Root>
-        </Layout.FixedContainer>
+                        <EmptyState.Body>
+                            <EmptyState.Title>
+                                {isAwaitingApproval
+                                    ? "Awaiting approval"
+                                    : "Waiting on presenter"}
+                            </EmptyState.Title>
+
+                            <EmptyState.Description>
+                                {isAwaitingApproval
+                                    ? "The presenter needs to approval you joining the room."
+                                    : "The presenter has not given you any actions to do yet."}
+                            </EmptyState.Description>
+                        </EmptyState.Body>
+                    </EmptyState.Container>
+                </EmptyState.Root>
+            </Layout.FixedContainer>
+        </>
     );
 }

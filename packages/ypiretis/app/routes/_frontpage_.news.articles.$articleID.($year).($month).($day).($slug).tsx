d1bc223da7a5cf9d8fb51b9913b6d23ee0e55606
oridim <125223080+oridim@users.noreply.github.com>
2025-08-19 02:28:25 -0400

    refactor: merge permalink functionality into main content view routes
    Yay less routes. I cannot believe I did not think of this before!

--- packages/ypiretis/app/routes/_frontpage_.news.articles.$articleID.($year).($month).($day).($slug).tsx
+++ packages/ypiretis/app/routes/_frontpage_.news.articles.$articleID.($year).($month).($day).($slug).tsx
@@ -1,16 +1,17 @@
-import {Span} from "@chakra-ui/react";
+import {Avatar, Span} from "@chakra-ui/react";
 
 import {data, redirect} from "react-router";
 
 import * as v from "valibot";
 
+import {findOnePublishedWithPoster} from "~/.server/services/articles_service";
 import {eq} from "~/.server/services/crud_service.filters";
-import {findOnePublished} from "~/.server/services/events_service";
 import {renderMarkdownForWeb} from "~/.server/services/markdown";
 
 import {slug, ulid} from "~/.server/utils/valibot";
 
 import DatetimeText from "~/components/common/datetime_text";
+import Links from "~/components/common/links";
 import Title from "~/components/common/title";
 
 import ContentSection from "~/components/frontpage/content_section";
@@ -18,37 +19,38 @@ import PageHero from "~/components/frontpage/page_hero";
 
 import {validateParams} from "~/guards/validation";
 
+import {ACCOUNT_PROVIDER_DOMAIN} from "~/utils/constants";
 import {NAVIGATOR_TIMEZONE} from "~/utils/navigator";
 import {number} from "~/utils/valibot";
 
-import {Route} from "./+types/_frontpage_.calendar.events.$eventID.$year.$month.$day.$slug";
+import {Route} from "./+types/_frontpage_.news.articles.$articleID.($year).($month).($day).($slug)";
 
 const LOADER_PARAMS_SCHEMA = v.object({
-    day: number,
+    articleID: ulid,
 
-    eventID: ulid,
+    day: v.optional(number),
 
-    month: number,
+    month: v.optional(number),
 
-    slug: slug,
+    slug: v.optional(slug),
 
-    year: number,
+    year: v.optional(number),
 });
 
 export async function loader(loaderArgs: Route.LoaderArgs) {
     const {
-        eventID,
-        day: inputDay,
-        month: inputMonth,
-        slug: inputSlug,
-        year: inputYear,
+        articleID,
+        day: inputDay = null,
+        month: inputMonth = null,
+        slug: inputSlug = null,
+        year: inputYear = null,
     } = validateParams(LOADER_PARAMS_SCHEMA, loaderArgs);
 
-    const event = await findOnePublished({
-        where: eq("eventID", eventID),
+    const article = await findOnePublishedWithPoster({
+        where: eq("articleID", articleID),
     });
 
-    if (event === null) {
+    if (article === null) {
         throw data("Not Found", {
             status: 404,
         });
@@ -57,11 +59,13 @@ export async function loader(loaderArgs: Route.LoaderArgs) {
     const {
         content,
         hasBeenEdited,
+        poster,
         publishedAt,
         slug: articleSlug,
         title,
         updatedAt,
-    } = event;
+    } = article;
+    const {accountID, firstName, lastName} = poster;
 
     const zonedPublishedAt = publishedAt.toZonedDateTimeISO(NAVIGATOR_TIMEZONE);
 
@@ -78,7 +82,7 @@ export async function loader(loaderArgs: Route.LoaderArgs) {
         publishedYear !== inputYear
     ) {
         return redirect(
-            `/calendar/events/${eventID}/${publishedYear}/${publishedMonth}/${publishedDay}/${articleSlug}`,
+            `/news/articles/${articleID}/${publishedYear}/${publishedMonth}/${publishedDay}/${articleSlug}`,
             {
                 status: 301,
             },
@@ -93,31 +97,43 @@ export async function loader(loaderArgs: Route.LoaderArgs) {
     const renderedContent = await renderMarkdownForWeb(content);
 
     return {
-        event: {
-            eventID,
+        article: {
+            articleID,
             publishedAtTimestamp,
             renderedContent,
             slug: articleSlug,
             title,
             updatedAtTimestamp,
         },
+
+        poster: {
+            accountID,
+            firstName,
+            lastName,
+        },
     };
 }
 
 export default function FrontpageNewsArticle(props: Route.ComponentProps) {
     const {loaderData} = props;
-    const {event} = loaderData;
+    const {article, poster} = loaderData;
 
     const {publishedAtTimestamp, renderedContent, title, updatedAtTimestamp} =
-        event;
+        article;
+
+    const {accountID, firstName, lastName} = poster;
+
+    const avatarSrc = `/images/avatars.${accountID}.webp`;
+    const fullName = `${firstName} ${lastName}`;
+    const email = `${accountID}@${ACCOUNT_PROVIDER_DOMAIN}`;
 
     return (
         <>
-            <Title title={`${title} :: /calendar`} />
+            <Title title={`${title} :: /news`} />
 
             <PageHero.Root>
                 <PageHero.Container>
-                    <PageHero.Text>/calendar</PageHero.Text>
+                    <PageHero.Text>/news</PageHero.Text>
                 </PageHero.Container>
             </PageHero.Root>
 
@@ -127,6 +143,25 @@ export default function FrontpageNewsArticle(props: Route.ComponentProps) {
                         <ContentSection.Title>{title}</ContentSection.Title>
 
                         <ContentSection.Description>
+                            <Avatar.Root blockSize="1.75em" inlineSize="1.75em">
+                                <Avatar.Fallback name={fullName} />
+
+                                <Avatar.Image
+                                    src={avatarSrc}
+                                    alt={`Avatar that represents ${fullName}.`}
+                                />
+                            </Avatar.Root>
+                            <address>
+                                <Span whiteSpace="pre"> {fullName} </Span>
+                                <Links.MailToLink
+                                    variant="prose"
+                                    to={email}
+                                    fontSize="sm"
+                                >
+                                    {email}
+                                </Links.MailToLink>
+                            </address>
+                            &nbsp;
                             <Span whiteSpace="pre">
                                 • Published{" "}
                                 <DatetimeText

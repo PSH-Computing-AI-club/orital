9de153d509cef5dde788961560efc6566f9f2426
oridim <125223080+oridim@users.noreply.github.com>
2025-08-19 06:00:43 -0400

    refactor: reuse UTC timestamp -> `Date` instance conversion

--- packages/ypiretis/app/utils/datetime.ts
+++ packages/ypiretis/app/utils/datetime.ts
@@ -8,21 +8,20 @@ import {NAVIGATOR_TIMEZONE} from "./navigator";
 export function toLocalISOCalendarString(timestamp: Date | number) {
     timestamp = typeof timestamp === "number" ? new Date(timestamp) : timestamp;
 
-    const year = timestamp.getFullYear();
-    const month = (timestamp.getMonth() + 1).toString().padStart(2, "0");
 
-    return `${year}-${month}`;
+export function toDate(timestamp: number | Date): Date {
+    return typeof timestamp === "number" ? new Date(timestamp) : timestamp;
 }
 
 export function toLocalISOString(timestamp: Date | number) {
-    timestamp = typeof timestamp === "number" ? new Date(timestamp) : timestamp;
+    const date = toDate(timestamp);
 
-    const year = timestamp.getFullYear();
-    const month = (timestamp.getMonth() + 1).toString().padStart(2, "0");
-    const day = timestamp.getDate().toString().padStart(2, "0");
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
 
-    const hours = timestamp.getHours().toString().padStart(2, "0");
-    const minutes = timestamp.getMinutes().toString().padStart(2, "0");
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
 
     return `${year}-${month}-${day}T${hours}:${minutes}`;
 }
@@ -42,7 +41,7 @@ export function toISOCalendarDayString(timestamp: number | Date): string {
 
 export function useDate(timestamp: number | Date): Date {
     return useMemo(() => {
-        return typeof timestamp === "number" ? new Date(timestamp) : timestamp;
+        return toDate(timestamp);
     }, [timestamp]);
 }
 

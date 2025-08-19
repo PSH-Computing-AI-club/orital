ceed5870a1f040d825ce31fe2be654d9815a5125
oridim <125223080+oridim@users.noreply.github.com>
2025-08-19 16:53:36 -0400

    chore: bikeshedding using long generic name as to made the difference between EST and EDT (example timezones) ambigous.

--- packages/ypiretis/app/utils/locale.ts
+++ packages/ypiretis/app/utils/locale.ts
@@ -70,7 +70,7 @@ export function formatCalendarTimestamp(
 
     const formatter = new Intl.DateTimeFormat(locale, {
         timeZone: timezone,
-        timeZoneName: "short",
+        timeZoneName: "longGeneric",
 
         month: "long",
         year: "numeric",

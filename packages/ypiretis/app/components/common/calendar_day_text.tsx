921004fc9757a6102c43e7e8e6e11f7682790503
oridim <125223080+oridim@users.noreply.github.com>
2025-08-20 09:28:55 -0400

    fix: custom stylings not being passed into time components

--- packages/ypiretis/app/components/common/calendar_day_text.tsx
+++ packages/ypiretis/app/components/common/calendar_day_text.tsx
@@ -1,14 +1,27 @@
+import type {BoxProps} from "@chakra-ui/react";
+import {Box} from "@chakra-ui/react";
+
 import type {IDateLike} from "~/utils/datetime";
 import {useFormattedCalendarDay} from "~/utils/locale";
 
-export interface IDatetimeTextProps {
+export interface IDatetimeTextProps extends BoxProps {
     readonly timestamp: IDateLike;
 }
 
 export default function CalendarDayText(props: IDatetimeTextProps) {
-    const {timestamp} = props;
+    const {timestamp, ...rest} = props;
 
     const {isoTimestamp, textualTimestamp} = useFormattedCalendarDay(timestamp);
 
-    return <time dateTime={isoTimestamp}>{textualTimestamp}</time>;
+    return (
+        <Box
+            as="time"
+            // @ts-expect-error - **HACK:** The `Box` component does not support the
+            // `dateTime` property via typing. But, it will pass it through nonetheless.
+            dateTime={isoTimestamp}
+            {...rest}
+        >
+            {textualTimestamp}
+        </Box>
+    );
 }
--- packages/ypiretis/app/components/common/datetime_text.tsx
+++ packages/ypiretis/app/components/common/datetime_text.tsx
@@ -1,19 +1,32 @@
+import type {BoxProps} from "@chakra-ui/react";
+import {Box} from "@chakra-ui/react";
+
 import type {IDateLike} from "~/utils/datetime";
 import type {IFormatDetail} from "~/utils/locale";
 import {useFormattedTimestamp} from "~/utils/locale";
 
-export interface IDatetimeTextProps {
+export interface IDatetimeTextProps extends BoxProps {
     readonly detail?: IFormatDetail;
 
     readonly timestamp: IDateLike;
 }
 
 export default function DatetimeText(props: IDatetimeTextProps) {
-    const {detail, timestamp} = props;
+    const {detail, timestamp, ...rest} = props;
 
     const {isoTimestamp, textualTimestamp} = useFormattedTimestamp(timestamp, {
         detail,
     });
 
-    return <time dateTime={isoTimestamp}>{textualTimestamp}</time>;
+    return (
+        <Box
+            as="time"
+            // @ts-expect-error - **HACK:** The `Box` component does not support the
+            // `dateTime` property via typing. But, it will pass it through nonetheless.
+            dateTime={isoTimestamp}
+            {...rest}
+        >
+            {textualTimestamp}
+        </Box>
+    );
 }

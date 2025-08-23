3aea4ebabbbeaade913722f43a6e9c8f29d89892
oridim <125223080+oridim@users.noreply.github.com>
2025-08-23 13:33:48 -0400

    feat: add timezone context to agenda feed

--- packages/ypiretis/app/components/frontpage/agenda_feed.tsx
+++ packages/ypiretis/app/components/frontpage/agenda_feed.tsx
@@ -1,6 +1,8 @@
 import type {StackProps} from "@chakra-ui/react";
 import {Flex, VStack} from "@chakra-ui/react";
 
+import {createContext, useContext, useMemo} from "react";
+
 import type {IDateLike} from "~/utils/datetime";
 
 import FeedStack from "./feed_stack";
@@ -9,6 +11,12 @@ import CalendarTextIcon from "../icons/calendar_text_icon";
 import DatetimeRangeText from "../common/datetime_range_text";
 import DatetimeText from "../common/datetime_text";
 import PinIcon from "../icons/pin_icon";
+
+const CONTEXT_AGENDA_FEED = createContext<IAgendaFeedContext | null>(null);
+
+interface IAgendaFeedContext {
+    readonly timezone: string;
+}
 
 export interface IEventTemplateContext {
     readonly event: IAgendaFeedEvent;
@@ -42,71 +50,93 @@ export interface IAgendaFeedProps extends StackProps {
     readonly timezone: string;
 }
 
+function useAgendaFeedContext(): IAgendaFeedContext {
+    const context = useContext(CONTEXT_AGENDA_FEED);
+
+    if (context === null) {
+        throw new ReferenceError(
+            `bad dispatch to 'useAgendaFeedContext' (not a child of 'CONTEXT_AGENDA_FEED.Provider')`,
+        );
+    }
+
+    return context;
+}
+
 export default function AgendaFeed(props: IAgendaFeedProps) {
     const {events, timezone, ...rest} = props;
 
+    const context = useMemo(() => {
+        return {
+            timezone,
+        } satisfies IAgendaFeedContext;
+    }, [timezone]);
+
     return (
-        <FeedStack.Root {...rest}>
-            {events.map((event) => {
-                const {
-                    description,
-                    endAtTimestamp,
-                    eventID,
-                    location,
-                    template,
-                    title,
-                    startAtTimestamp,
-                } = event;
+        <CONTEXT_AGENDA_FEED.Provider value={context}>
+            <FeedStack.Root {...rest}>
+                {events.map((event) => {
+                    const {
+                        description,
+                        endAtTimestamp,
+                        eventID,
+                        location,
+                        template,
+                        title,
+                        startAtTimestamp,
+                    } = event;
 
-                const url = template({event});
+                    const url = template({event});
 
-                return (
-                    <FeedStack.Item key={eventID}>
-                        <FeedCard.Root>
-                            <FeedCard.Body>
-                                <FeedCard.Title to={url.toString()}>
-                                    {title}
-                                </FeedCard.Title>
+                    return (
+                        <FeedStack.Item key={eventID}>
+                            <FeedCard.Root>
+                                <FeedCard.Body>
+                                    <FeedCard.Title to={url.toString()}>
+                                        {title}
+                                    </FeedCard.Title>
 
-                                <FeedCard.Description>
-                                    <VStack gap="1" alignItems="start">
-                                        <Flex lineHeight="short">
-                                            <CalendarTextIcon />
-                                            &nbsp;
-                                            {endAtTimestamp ? (
-                                                <DatetimeRangeText
-                                                    timezone={timezone}
-                                                    startAtTimestamp={
-                                                        startAtTimestamp
-                                                    }
-                                                    endAtTimestamp={
-                                                        endAtTimestamp
-                                                    }
-                                                    detail="long"
-                                                />
-                                            ) : (
-                                                <DatetimeText
-                                                    timezone={timezone}
-                                                    timestamp={startAtTimestamp}
-                                                    detail="long"
-                                                />
-                                            )}
-                                        </Flex>
+                                    <FeedCard.Description>
+                                        <VStack gap="1" alignItems="start">
+                                            <Flex lineHeight="short">
+                                                <CalendarTextIcon />
+                                                &nbsp;
+                                                {endAtTimestamp ? (
+                                                    <DatetimeRangeText
+                                                        timezone={timezone}
+                                                        startAtTimestamp={
+                                                            startAtTimestamp
+                                                        }
+                                                        endAtTimestamp={
+                                                            endAtTimestamp
+                                                        }
+                                                        detail="long"
+                                                    />
+                                                ) : (
+                                                    <DatetimeText
+                                                        timezone={timezone}
+                                                        timestamp={
+                                                            startAtTimestamp
+                                                        }
+                                                        detail="long"
+                                                    />
+                                                )}
+                                            </Flex>
 
-                                        <Flex lineHeight="short">
-                                            <PinIcon />
-                                            &nbsp;
-                                            {location ?? "TBD"}
-                                        </Flex>
-                                    </VStack>
-                                </FeedCard.Description>
+                                            <Flex lineHeight="short">
+                                                <PinIcon />
+                                                &nbsp;
+                                                {location ?? "TBD"}
+                                            </Flex>
+                                        </VStack>
+                                    </FeedCard.Description>
 
-                                <FeedCard.Text>{description}</FeedCard.Text>
-                            </FeedCard.Body>
-                        </FeedCard.Root>
-                    </FeedStack.Item>
-                );
-            })}
-        </FeedStack.Root>
+                                    <FeedCard.Text>{description}</FeedCard.Text>
+                                </FeedCard.Body>
+                            </FeedCard.Root>
+                        </FeedStack.Item>
+                    );
+                })}
+            </FeedStack.Root>
+        </CONTEXT_AGENDA_FEED.Provider>
     );
 }
